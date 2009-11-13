97e5bc4a8cd276506039a69cc60c5f2c24947104
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2009-11-13 11:54:21 +0000

    "Create abstract data class"

---
+++ server/lib/data.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Abstract class for all data classes (MySQL, Memcache...).
+ * @package stalker_portal
+ */
+abstract class Data
+{
+    abstract function getData(){}
+    
+    abstract function insertData(){}
+    
+    abstract function updateData(){}
+    
+    abstract function deleteData(){}
+    
+    protected function getKey($prefix, $data = array()){
+        
+    }
+    
+    public function getFirstData(){
+        $result = call_user_func_array(array($this, 'getData'), func_get_args());
+        
+        if (is_array($result) && count($result) > 0){
+            return $result[0];
+        }
+        
+        return $result;
+    }
+    
+}
+?>
