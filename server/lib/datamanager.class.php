301b5171a42a9e96d9e4a587bc42cfc6c95156fb
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2009-11-13 09:40:58 +0000

    "Add DataManager class"

---
+++ server/lib/datamanager.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Factory of Data objects.
+ * @package stalker_portal
+ */
+
+class DataManager
+{
+    private static $map = array(
+        'user' => array('get_profile'),
+        'itv'  => array(),
+        'vod'  => array(),
+        'karaoke'  => array(),
+    );
+    
+    public static function create($type){
+        $type = $type || '';
+        
+        foreach (self::$map as $name => $types){
+            if (in_array($type, $types)){
+                $class = ucfirst($name).'Data';
+                if (class_exists($class)){
+                    return new $class;
+                }
+            }
+        }
+    }
+}
+?>
