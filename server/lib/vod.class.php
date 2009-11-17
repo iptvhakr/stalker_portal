fb2cb0de3263a73e3e0da33aa32185b5467f31fe
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2009-11-17 10:31:22 +0000

    "Added setNotEnded, setPlayed, setFav, getFav, delFav methods"

--- server/lib/vod.class.php
+++ server/lib/vod.class.php
@@ -88,7 +88,206 @@ class Vod
     
     public function setPlayed(){
         
+        $video_id   = intval($_REQUEST['video_id']);
+        $storage_id = intval($_REQUEST['storage_id']);
+        
+        if ($day <= date("j")){
+            $field_name = 'count_first_0_5';
+        }else{
+            $field_name = 'count_second_0_5';
+        }
+        
+        $video = $this->db->getFirstData('video', array('id' => $video_id));
+        
+        $this->db->updateData('video',
+                               array(
+                                    $field_name   => $video[$field_name] + 1,
+                                    'count'       => $video['count'] + 1,
+                                    'last_played' => 'NOW()'
+                               ),
+                               array('id' => $video_id));
+        
+        $this->db->insertData('played_video',
+                               array(
+                                    'video_id' => $video_id,
+                                    'uid'      => $this->stb->id,
+                                    'storage'  => $storage_id,
+                                    'playtime' => 'NOW()'
+                               ));
+        
+        $this->db->updateData('users',
+                               array('time_last_play_video' => 'NOW()'),
+                               array('id' => $this->stb->id));
+        
+        $today_record = $this->db->getFirstData('daily_played_video', array('date' => 'CURDATE()'));
+        
+        if (empty($today_record)){
+            
+            $this->db->insertData('daily_played_video',
+                                   array(
+                                        'count' => 1,
+                                        'date'  => 'CURDATE()'
+                                   ));
+            
+        }else{
+            
+            $this->db->updateData('daily_played_video',
+                                   array(
+                                        'count' => $today_record['count'] + 1,
+                                        'date'  => 'NOW()'
+                                   ));
+            
+        }
+        
+        $played_video = $this->db->getData('stb_played_video',
+                            array(
+                                'uid' => $this->stb->id,
+                                'video_id' => $video_id
+                            ));
+        
+        if (empty($played_video)){
+            
+            $this->db->insertData('stb_played_video',
+                                   array(
+                                        'uid'      => $this->stb->id,
+                                        'video_id' => $video_id,
+                                        'playtime' => 'NOW()'
+                                   ));
+            
+        }else{
+            
+            $this->db->updateData('stb_played_video',
+                                   array('playtime' => 'NOW()'),
+                                   array(
+                                        'uid'      => $this->stb->id,
+                                        'video_id' => $video_id
+                                   ));
+            
+        }
+        
+        return true;
     }
+    
+    public function setFav(){
+        
+        $new_id = intval($_REQUEST['video_id']);
+
+        $fav_video = $this->getFav();
+        
+        if (!is_array($fav_video)){
+            $this->db->insertData('fav_vclub',
+                                   array(
+                                        'uid'       => $this->stb->id,
+                                        'fav_video' => serialize(array($new_id)),
+                                        'addtime'   => 'NOW()'
+                                   ));
+             return true;                      
+        }
+        
+        if (!in_array($new_id, $fav_video)){
+            
+            $fav_video[] = $new_id;
+            $fav_video_s = serialize($fav_video);
+            
+            $this->db->updateData('fav_vclub',
+                                   array(
+                                        'fav_video' => $fav_video_s,
+                                        'edittime'  => 'NOW()'),
+                                   array('uid' => $this->stb->id));
+            
+        }
+        
+        return true;
+    }
+    
+    public function getFav(){
+        
+        $fav_video_arr = $this->db->getFirstData('fav_vclub', array('uid' => $this->stb->id));
+        
+        if (empty($fav_video_arr)){
+            return null;
+        }
+        
+        $fav_video = unserialize($fav_video_arr['fav_video']);
+        
+        if (!is_array($fav_video)){
+            $fav_video = array();
+        }
+        
+        return $fav_video;
+    }
+    
+    public function delFav(){
+        
+        $del_id = intval($_REQUEST['video_id']);
+        
+        $fav_video = $this->getFav();
+        
+        if (is_array($fav_video)){
+
+            if (in_array($del_id, $fav_video)){
+                
+                unset($fav_video[array_search($del_id, $fav_video)]);
+                
+                $fav_video_s = serialize($fav_video);
+                
+                $this->db->updateData('fav_vclub',
+                                       array(
+                                            'fav_video' => $fav_video_s,
+                                            'edittime'  => 'NOW()'
+                                       ),
+                                       array('uid' => $this->stb->id));
+                
+            }
+        }
+        
+        return true;
+    }
+    
+    public function setNotEnded(){
+        
+        $video_id   = intval($_REQUEST['video_id']);
+        $series     = intval($_REQUEST['series']);
+        $end_time   = intval($_REQUEST['end_time']);
+        
+        $not_ended = $this->db->getFirstData('vclub_not_ended',
+                                              array(
+                                                   'uid' => $this->stb->id,
+                                                   'video_id' => $video_id
+                                              ));
+        
+        
+        
+        if (empty($not_ended)){
+
+            $this->db->insertData('vclub_not_ended',
+                                   array(
+                                        'uid'      => $this->stb->id,
+                                        'video_id' => $video_id,
+                                        'series'   => $series,
+                                        'end_time' => $end_time,
+                                        'added'    => 'NOW()'
+                                   ));
+            
+        }else{
+            
+            $this->db->updateData('vclub_not_ended',
+                                   array(
+                                        'series'   => $series,
+                                        'end_time' => $end_time,
+                                        'added'    => 'NOW()'
+                                   ),
+                                   array(
+                                        'uid'      => $this->stb->id,
+                                        'video_id' => $video_id
+                                   ));
+            
+        }
+        
+        return true;
+    }
+    
+    
 }
 
 ?>
