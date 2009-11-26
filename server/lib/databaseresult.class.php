3125990313eb0465b13a4e3b25cf0cbbf80fb5f2
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2009-11-26 10:17:49 +0000

    "Add DatabaseResult class"

---
+++ server/lib/databaseresult.class.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Abstract class for databare result.
+ * 
+ * @package stalker_portal
+ * @author zhurbitsky@gmail.com
+ */
+
+abstract class DatabaseResult
+{
+    protected $result;
+    
+    protected $total_rows  = 0;
+    protected $current_row = 0;
+    protected $insert_id;
+    protected $sql;
+    
+    abstract public function __construct($result, $sql, $link);
+    
+    abstract public function __destruct();
+    
+    abstract public function all();
+    
+    public function insert_id(){
+        
+        return $this->insert_id;
+    }
+    
+    public function get($name = null){
+        
+        $row = $this->current();
+        
+        if ($name === null){
+            return $row;
+        }else{
+            return $row[$name];
+        }
+	}
+	
+	public function offsetExist($offset){
+	    
+	    return ($offset >=0 && $offset < $this->total_rows);
+	}
+	
+	public function count(){
+	    
+        return $this->total_rows;
+	}
+	
+	public function rewind(){
+	    
+	    $this->current_row = 0;
+	    return $this;
+	}
+	
+}
+?>
