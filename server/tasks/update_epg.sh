d66c7b15a205957df05c47874946576a27a03fe9
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-01-25 15:34:58 +0000

    "Add force update"

--- server/tasks/update_epg.sh
+++ server/tasks/update_epg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget -q -O - http://localhost/stalker_portal/server/tasks/update_epg.php && echo 1 || echo 0
+wget -q -O - http://localhost/stalker_portal/server/tasks/update_epg.php?$1 && echo 1 || echo 0
