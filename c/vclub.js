86d476bbcbd9d3a23637511ffb892e1c31d89ddd
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-02-23 13:34:39 +0000

    "Add vclub module"

--- c/vclub.js
+++ c/vclub.js
@@ -2,3 +2,31 @@
  * Video Club modile.
  */
 
+//(function(){
+    
+    var genres = ['ИНФОРМАЦИОННЫЙ',
+            'РАЗВЛЕЧЕНИЯ',
+            'ДЕТСКИЙ',
+            'КИНО',
+            'НАУКА',
+            'СПОРТ',
+            'МУЗЫКА',
+            'БИЗНЕС',
+            'КУЛЬТУРА',
+            'НАШЕ',
+            'ЗАРУБЕЖНОЕ']
+    
+    var vclub_constructor = function(){
+        
+    };
+    
+    vclub_constructor.prototype = new Layer();
+    
+    var vclub = new vclub_constructor();  
+    
+    vclub.init();
+    vclub.init_sidebar();
+    vclub.sidebar.init_items("genre", genres);
+    
+    
+//})()
