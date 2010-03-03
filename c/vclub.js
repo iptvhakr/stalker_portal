3a59d8d0e4e2af04358becf3c88d44f52a810299
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-03-03 09:13:54 +0000

    "Working vclub list & sidebar"

--- c/vclub.js
+++ c/vclub.js
@@ -3,30 +3,44 @@
  */
 
 //(function(){
-    
-    var genres = ['ИНФОРМАЦИОННЫЙ',
-            'РАЗВЛЕЧЕНИЯ',
-            'ДЕТСКИЙ',
-            'КИНО',
-            'НАУКА',
-            'СПОРТ',
-            'МУЗЫКА',
-            'БИЗНЕС',
-            'КУЛЬТУРА',
-            'НАШЕ',
-            'ЗАРУБЕЖНОЕ']
+
+    var genres = [{"id" : 1, "title" : "ИНФОРМАЦИОННЫЙ"},
+            {"id" : 2, "title" : "РАЗВЛЕЧЕНИЯ"},
+            {"id" : 3, "title" : "ДЕТСКИЙ"},
+            {"id" : 4, "title" : "КИНО"},
+            {"id" : 5, "title" : "НАУКА"},
+            {"id" : 6, "title" : "СПОРТ"},
+            {"id" : 7, "title" : "МУЗЫКА"},
+            {"id" : 8, "title" : "БИЗНЕС"},
+            {"id" : 9, "title" : "КУЛЬТУРА"},
+            {"id" : 10, "title" : "НАШЕ"},
+            {"id" : 11, "title" : "ЗАРУБЕЖНОЕ"}]
     
     var vclub_constructor = function(){
         
-    };
+        this.row_blocks  = ['hd', 'sd', 'fav', 'lock', 'name'];
+        
+        this.load_params = {
+            'type'   : 'vod',
+            'action' : 'get_by_name'
+        }
+    }
+    
+    stb.ajax_loader = 'http://bb2.sandbox/stalker_portal/server/load.php'
     
     vclub_constructor.prototype = new Layer();
     
-    var vclub = new vclub_constructor();  
+    var vclub = new vclub_constructor();
+    vclub.bind();
     
     vclub.init();
+    stb.key_lock = false;
+    
+    vclub.on = true;
+    
     vclub.init_sidebar();
     vclub.sidebar.init_items("genre", genres);
+    vclub.sidebar.bind();
     
     
 //})()
