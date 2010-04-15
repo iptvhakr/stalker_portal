aeb7d9a5986a88012774f85e1e2c2ab9f5089753
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-04-15 11:46:25 +0000

    "Add add_del_fav"

--- c/tv.js
+++ c/tv.js
@@ -13,6 +13,14 @@
             'action' : 'get_ordered_list'
         };
         
+        this.preview_pos_map = [
+            {"mode" : 576, "xsize" : 320, "ysize" : 256, "x" : 350, "y" : 74},
+            {"mode" : 720, "xsize" : 569, "ysize" : 320, "x" : 622, "y" : 93},
+            {"mode" : 1080, "xsize" : 854, "ysize" : 480, "x" : 933, "y" : 139}
+        ];
+        
+        this.preview_pos = this.preview_pos_map[this.preview_pos_map.getIdxByVal("mode", parseInt(stb.video_mode))];
+        
         this.superclass = Layer.prototype;
         
         this.sort_menu = {};
@@ -20,7 +28,7 @@
         this.view_menu = {};
         
         this.row_callback_timer;
-        this.row_callback_timeout = 1000;
+        this.row_callback_timeout = 100;
         
         this._show = function(genre){
             
@@ -28,9 +36,37 @@
             
             this.load_params['genre'] = genre.id;
             
+            this.update_header_path([{"alias" : "genre", "item" : genre.title}]);
+            
             this.sort_menu.action();
+            
             this.superclass.show.call(this);
+            
+            try{
+                if (this.cur_view == 'short'){
+                    stb.SetTopWin(1);
+                    stb.SetViewport(this.preview_pos.xsize, this.preview_pos.ysize, this.preview_pos.x, this.preview_pos.y);
+                }else{
+                    stb.SetTopWin(0);
+                }
+            }catch(e){
+                _debug(e);
+            }
         }
+        
+        this.hide = function(do_not_reset){
+            _debug('tv.hide', do_not_reset);
+            
+            try{
+                this.superclass.hide.call(this, do_not_reset);
+            
+                _debug('SetTopWin');
+                stb.SetTopWin(0);
+                stb.SetPIG(1, -1, -1, -1);
+            }catch(e){
+                _debug(e);
+            }
+        };
         
         this.bind = function(){
             this.superclass.bind.apply(this);
@@ -50,7 +86,9 @@
             
             stb.player.prev_layer = this;
             
-            stb.player.play(this.data_items[this.cur_row]);
+            if (!stb.player.on){
+                stb.player.play(this.data_items[this.cur_row]);
+            }
         };
         
         this.init_sort_menu = function(map, options){
@@ -81,6 +119,37 @@
             }
         };
         
+        this.set_wide_container = function(){
+            
+            if (this.cur_view == 'short'){
+                stb.player.stop();
+            }
+            
+            this.superclass.set_wide_container.apply(this);
+
+            try{
+                stb.SetTopWin(0);
+                stb.SetPIG(1, -1, -1, -1);
+            }catch(e){
+                _debug(e);
+            }
+        };
+        
+        this.set_short_container = function(){
+            
+            this.superclass.set_short_container.apply(this);
+            
+            try{
+                _debug('this.preview_pos', this.preview_pos);
+                stb.SetTopWin(1);
+                stb.SetViewport(this.preview_pos.xsize, this.preview_pos.ysize, this.preview_pos.x, this.preview_pos.y);
+            }catch(e){
+                _debug(e);
+            }
+            
+            this.fill_short_info(this.data_items[this.cur_row]);
+        };
+        
         this.init_short_info = function(){
             this.info_box = create_block_element('', this.main_container);
             
@@ -91,7 +160,19 @@
         this.fill_short_info = function(item){
             _debug('fill_short_info');
             
-            this.short_info_box.innerHTML = '';
+            var epg = '';
+            
+            for (var i=0; i<item.epg.length; i++){
+                epg += '<span>' + item.epg[i].t_time + ' - </span>' + item.epg[i].name + '<br>';
+            }
+            
+            this.short_info_box.innerHTML = epg;
+            
+            try{
+                stb.player.play(item);
+            }catch(e){
+                _debug(e);
+            }
         };
         
         this.shift_row_callback = function(item){
@@ -107,6 +188,56 @@
             },
             this.row_callback_timeout);
         };
+        
+        this.add_to_fav = function(){
+            _debug('tv.add_to_fav');
+            
+            _debug('this.player.fav_channels before', stb.player.fav_channels_ids);
+            
+            stb.player.fav_channels_ids.push(this.data_items[this.cur_row].id);
+            
+            _debug('this.player.fav_channels after', stb.player.fav_channels_ids);
+            
+            this.data_items[this.cur_row].fav = 1;
+            
+            this.map[this.cur_row].fav_block.show();
+            this.active_row.fav_block.show();
+            
+            stb.player.save_fav_ids();
+        };
+        
+        this.del_from_fav = function(){
+            _debug('tv.del_from_fav');
+            
+            _debug('this.player.fav_channels before', stb.player.fav_channels_ids);
+            
+            var fav_idx = stb.player.fav_channels_ids.indexOf(this.data_items[this.cur_row].id);
+            
+            var removed_idx = stb.player.fav_channels_ids.splice(fav_idx, 1);
+            
+            _debug('removed_idx', removed_idx);
+            
+            _debug('this.player.fav_channels after', stb.player.fav_channels_ids);
+            
+            this.data_items[this.cur_row].fav = 0;
+            
+            this.map[this.cur_row].fav_block.hide();
+            this.active_row.fav_block.hide();
+            
+            stb.player.save_fav_ids();
+        };
+        
+        this.add_del_fav = function(){
+            _debug('tv.add_del_fav');
+            
+            if(this.data_items[this.cur_row].fav){
+                this.del_from_fav();
+            }else{
+                this.add_to_fav();
+            }
+        };
+        
+        
     }
     
     tv_constructor.prototype = new Layer();
@@ -125,7 +256,7 @@
     tv.init_color_buttons([
         {"label" : "ОТОБРАЖЕНИЕ", "cmd" : tv.view_switcher},
         {"label" : "СОРТИРОВКА", "cmd" : tv.sort_menu_switcher},
-        {"label" : "ИЗБРАННОЕ", "cmd" : tv.manage_fav},
+        {"label" : "ИЗБРАННОЕ", "cmd" : tv.add_del_fav},
         {"label" : "", "cmd" : ''}
     ]);
     
