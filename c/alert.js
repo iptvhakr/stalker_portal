6ab59c0b98962daed727a7e185791230e70e3995
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-05-20 08:03:17 +0000

    "Add set_callback for hide, modify for info msg"

--- c/alert.js
+++ c/alert.js
@@ -3,16 +3,24 @@
  * @constructor
  */
 
-function _alert(){
+function _alert(type){
     
     this.on = false;
     
     this.dom_obj = {};
     
+    if (type){
+        this.type = type
+    }else{
+        this.type = 'alert';
+    }
+    
     this.txt_container = {};
     
     this.hide_timer = 2000;
     this.hide_to;
+    
+    this.callback = function(){};
     
     this.init();
 }
@@ -23,7 +31,7 @@ _alert.prototype.init = function(){
         
         var alert_outer = create_block_element('alert_outer', this.dom_obj);
         
-        var alert_container = create_block_element('alert_container', alert_outer);
+        var alert_container = create_block_element(this.type + '_container', alert_outer);
         
         var table = document.createElement('table');
         alert_container.appendChild(table);
@@ -54,7 +62,7 @@ _alert.prototype.init = function(){
         td.setClass('mb_info_main');
         tr.appendChild(td);
         
-        var icon = create_block_element('ico_alert', td);
+        var icon = create_block_element('ico_'+this.type, td);
         
         this.txt_container = create_inline_element('', td);
         
@@ -80,9 +88,11 @@ _alert.prototype.show = function(txt){
     this.txt_container.innerHTML = txt;
     
     this.dom_obj.show();
-    this.dom_obj.on = true;
+    this.on = true;
     
-    this.t_hide();
+    if (this.type == 'alert'){
+        this.t_hide();
+    }
 }
 
 _alert.prototype.t_hide = function(){
@@ -100,6 +110,18 @@ _alert.prototype.hide = function(){
     _debug('_alert.hide');
     
     this.dom_obj.hide();
-    this.dom_obj.on = false;
+    this.on = false;
+    
+    this.callback && this.callback();
+    this.callback = function(){};
 }
 
+_alert.prototype.set_callback = function(callback){
+    _debug('_alert.set_callback');
+    
+    this.callback = callback;
+}
+
+_alert.prototype.bind = function(){
+    this.hide.priority_bind(key.OK, this);
+}
