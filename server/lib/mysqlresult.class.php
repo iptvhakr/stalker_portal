b86eb2776a028cc2980e515c2536c88569a126bc
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-06-03 15:16:34 +0000

    "Remove count_rows add counter"

--- server/lib/mysqlresult.class.php
+++ server/lib/mysqlresult.class.php
@@ -93,9 +93,9 @@ class MysqlResult extends DatabaseResult
 	    return mysql_fetch_assoc($this->result);
 	}
 	
-	public function count_rows(){
-	    return $this->total_rows;
+	public function counter(){
+	    
+	    return $this->first('count(*)');
 	}
-	
 }
 ?>
