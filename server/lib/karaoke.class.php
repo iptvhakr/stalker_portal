f34aaa6146f6bca758d1981ad49c099fe2e3d92f
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-06-08 10:32:01 +0000

    "Add getData, getOrderedList"

--- server/lib/karaoke.class.php
+++ server/lib/karaoke.class.php
@@ -6,44 +6,124 @@
  * @author zhurbitsky@gmail.com
  */
 
-class Karaoke
+class Karaoke extends AjaxResponse 
 {
-    
-    private $db;
-    private $stb;
-    
     private static $instance = NULL;
     
     public static function getInstance(){
         if (self::$instance == NULL)
         {
-            self::$instance = new Karaoke();
+            self::$instance = new self;
         }
         return self::$instance;
     }
     
     public function __construct(){
-        $this->db  = Mysql::getInstance();
-        $this->stb = Stb::getInstance();
+        parent::__construct();
     }
     
     public function createLink(){
         
-        preg_match("/auto \/media\/(\d+).mpg$/", $data_req, $tmp_arr);
+        preg_match("/\/media\/(\d+).mpg$/", $_REQUEST['cmd'], $tmp_arr);
         
         $media_id = $tmp_arr[1];
 
         $master = new KaraokeMaster();
         
         try {
-            $res['data'] = $master->play($media_id);
+            $res = $master->play($media_id);
         }catch (Exception $e){
-            echo trigger_error($e->getMessage());
+            trigger_error($e->getMessage());
         }
         
         var_dump($res);
         
         return $res;
+    }
+    
+    private function getData(){
+        
+        $offset = $this->page * MAX_PAGE_ITEMS;
+        
+        $where = array('status' => 1);
+        
+        if (!$this->stb->isModerator()){
+            $where['accessed'] = 1;
+        }
+        
+        $like = array();
+        
+        if (@$_REQUEST['abc'] && @$_REQUEST['abc'] !== '*'){
+            
+            $letter = $_REQUEST['abc'];
+            
+            if (@$_REQUEST['sortby'] == 'name'){
+                $like = array('karaoke.name' => $letter.'%');
+            }else{
+                $like = array('karaoke.singer' => $letter.'%');
+            }
+        }
+        
+        if (@$_REQUEST['search']){
+            
+            $letters = $_REQUEST['search'];
+            
+            $search['karaoke.name']   = '%'.$letters.'%';
+            $search['karaoke.singer'] = '%'.$letters.'%';
+        }
+        
+        return $this->db
+                        ->from('karaoke')
+                        ->where($where)
+                        ->like($like)
+                        ->like($search, 'OR ')
+                        ->limit(MAX_PAGE_ITEMS, $offset);
+    }
+    
+    public function getOrderedList(){
+     
+        $result = $this->getData();
+        
+        if (@$_REQUEST['sortby']){
+            $sortby = $_REQUEST['sortby'];
+            
+            if ($sortby == 'name'){
+                $result = $result->orderby('karaoke.name');
+            }elseif ($sortby == 'singer'){
+                $result = $result->orderby('karaoke.singer');
+            }
+            
+        }else{
+            $result = $result->orderby('karaoke.singer');
+        }
+        
+        $this->setResponseData($result);
+        
+        return $this->getResponse('prepareData');
+    }
+    
+    public function prepareData(){
+        
+        for ($i = 0; $i < count($this->response['data']); $i++){
+            
+            $this->response['data'][$i]['cmd'] = '/media/'.$this->response['data'][$i]['id'].'.mpg';
+        }
+        
+        return $this->response;
+    }
+    
+    public function getAbc(){
+        
+        $abc = array();
+        
+        foreach ($this->abc as $item){
+            $abc[] = array(
+                        'id'    => $item,
+                        'title' => $item
+                     );
+        }
+        
+        return $abc;
     }
 }
 
