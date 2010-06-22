c76f4eed6a29afa4fb60dc6b4fa939f8590d125a
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-06-22 11:53:08 +0000

    "Add setClaimGlobal"

--- server/lib/ajaxresponse.class.php
+++ server/lib/ajaxresponse.class.php
@@ -94,6 +94,70 @@ class AjaxResponse
         $dir_path .= '/'.$id.'.jpg';
         return $dir_path;
     }
+    
+    protected function setClaimGlobal($media_type){
+        
+        $id   = intval($_REQUEST['id']);
+        $type = $_REQUEST['real_type'];
+        
+        $this->db->insert('media_claims_log',
+                          array(
+                              'media_type' => $media_type,
+                              'media_id'   => $id,
+                              'type'       => $type,
+                              'uid'        => $this->stb->id,
+                              'added'      => 'NOW()'
+                          ));
+                          
+        $total_media_claims = $this->db->from('media_claims')->where(array('media_type' => $media_type, 'media_id' => $id))->get()->first();
+        
+        $sound_counter = 0;
+        $video_counter = 0;
+        
+        if ($type == 'video'){
+            $video_counter++;
+        }else{
+            $sound_counter++;
+        }
+        
+        if (!empty($total_media_claims)){
+            $this->db->update('media_claims',
+                              array(
+                                  'sound_counter' => $total_media_claims['sound_counter'] + $sound_counter,
+                                  'video_counter' => $total_media_claims['video_counter'] + $video_counter,
+                              ),
+                              array(
+                                  'media_type' => $media_type,
+                                  'media_id'   => $id
+                              ));
+        }else{
+            $this->db->insert('media_claims',
+                              array(
+                                  'sound_counter' => $sound_counter,
+                                  'video_counter' => $video_counter,
+                                  'media_type'    => $media_type,
+                                  'media_id'      => $id
+                              ));
+        }
+        
+        $total_daily_claims = $this->db->from('daily_media_claims')->where(array('date' => 'CURDATE()'))->get()->first();
+        
+        if (!empty($total_daily_claims)){
+            $this->db->update('daily_media_claims',
+                              array(
+                                  $media_type.'_sound' => $total_daily_claims[$media_type.'_sound'] + $sound_counter,
+                                  $media_type.'_video' => $total_daily_claims[$media_type.'_video'] + $video_counter
+                              ),
+                              array('date' => 'CURDATE()'));
+        }else{
+            $this->db->insert('daily_media_claims',
+                              array(
+                                  $media_type.'_sound' => $sound_counter,
+                                  $media_type.'_video' => $video_counter,
+                                  'date'               => 'CURDATE()'
+                              ));
+        }
+    }
 }
 
 ?>
