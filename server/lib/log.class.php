5e775160dd94f1107e00ac603bcf6fc243da9901
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-07-06 13:48:29 +0000

    "Add log.class.php"

---
+++ server/lib/log.class.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Log engine.
+ * 
+ * @package stalker_portal
+ * @author zhurbitsky@gmail.com
+ */
+
+class Log
+{
+    private $db;
+    private $stb;
+    
+    private static $instance = NULL;
+    
+    public static function getInstance(){
+        if (self::$instance == NULL)
+        {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+    
+    public function __construct(){
+        $this->db  = Mysql::getInstance();
+        $this->stb = Stb::getInstance();
+    }
+    
+    public function savePageGenerationTime($time){
+        $time = $time*1000;
+        
+        if ($time >= 500){
+            $default_row = '500ms';
+        }elseif ($time >= 400){
+            $default_row = '400ms';
+        }elseif ($time >= 300){
+            $default_row = '300ms';
+        }elseif ($time >= 200){
+            $default_row = '200ms';
+        }elseif ($time >= 100){
+            $default_row = '100ms';
+        }else{
+            $default_row = '0ms';
+        }
+        
+        $this->db->update('generation_time',
+                          array('counter' => 'counter+1'),
+                          array('time' => $default_row));
+        
+    }
+}
+
+?>
