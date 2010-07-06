bdb254af1acd55972486a184f2442d70938e50a5
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-07-06 13:54:52 +0000

    "Add generation_time table"

--- db/db.sql
+++ db/db.sql
@@ -1056,3 +1056,11 @@ CREATE TABLE `testers`(
     UNIQUE KEY (`mac`),
     PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `generation_time`(
+    `time` varchar(32),
+    `counter` int unsigned NOT NULL default 0,
+    INDEX(`time`)
+) ENGINE=MEMORY;
+
+INSERT INTO `generation_time` (`time`) values ('0ms'), ('100ms'), ('200ms'), ('300ms'), ('400ms'), ('500ms');
