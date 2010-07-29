5345b70a04d74ce03a19e1b14aee2a4f4bb90e96
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-07-29 12:50:48 +0000

    "Add curweather.js"

---
+++ c/curweather.js
@@ -0,0 +1,48 @@
+/**
+ *
+ */
+
+(function(){
+    
+    var curweather = {
+        
+        //current : {},
+        dom_obj : {},
+        
+        init : function(){
+            
+            this.dom_obj = create_block_element('curweather_block', main_menu.dom_obj);
+        },
+        
+        set : function(weather){
+            _debug('curweather.set', weather);
+            this.current = weather;
+            
+            if (main_menu){
+                this.render();
+            }
+        },
+        
+        render : function(){
+            _debug('curweather.render');
+            
+            var cur = '<img src="i/weather/' + this.current.pict + '"/><br>';
+            cur += this.current.t +'&deg; C<br>';
+            cur += this.current.cloud_str + '<br>';
+            cur += 'Комфорт: ' + this.current.t_flik +'&deg; C<br>';
+            cur += 'Давление: ' + this.current.p + ' мм. рт.ст.<br>';
+            cur += 'Ветер: '+ this.current.w_rumb_str + ', ' + this.current.w + ' м/с<br>';
+            cur += 'Влажность: '+ this.current.h + '%<br>';
+            
+            this.dom_obj.innerHTML = cur;
+        }
+        
+    }
+    
+    curweather.init();
+    
+    module.curweather = curweather;
+    
+    loader.next();
+    
+})();
