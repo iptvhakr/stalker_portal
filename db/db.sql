66daa007448c3d3f0ff5df165270e0e6e6b56004
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-07-29 14:22:06 +0000

    "Add table weatherco_cache"

--- db/db.sql
+++ db/db.sql
@@ -1064,3 +1064,12 @@ CREATE TABLE `generation_time`(
 ) ENGINE=MEMORY;
 
 INSERT INTO `generation_time` (`time`) values ('0ms'), ('100ms'), ('200ms'), ('300ms'), ('400ms'), ('500ms');
+
+CREATE TABLE `weatherco_cache`(
+    `id` int NOT NULL auto_increment,
+    `current` text,
+    `forecast` text,
+    `url` varchar(128),
+    `updated` datetime,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
