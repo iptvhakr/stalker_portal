45d7b20df316044447fbe2e38e52717fb51da188
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-08-16 13:59:33 +0000

    "Add ru.js"

---
+++ c/l/ru.js
@@ -0,0 +1,93 @@
+var word = [];
+
+// weather
+word['weather_comfort'] = 'Комфорт';
+word['weather_pressure'] = 'Давление';
+word['weather_mmhg'] = 'мм. рт.ст.';
+word['weather_wind'] = 'Ветер';
+word['weather_speed'] = 'м/с';
+word['weather_humidity'] = 'Влажность';
+
+// karaoke
+word['karaoke_view'] = 'ОТОБРАЖЕНИЕ';
+word['karaoke_sort'] = 'СОРТИРОВКА';
+word['karaoke_search'] = 'ПОИСК';
+word['karaoke_sampling'] = 'ВЫБОРКА';
+word['karaoke_by_letter'] = 'ПО БУКВЕ';
+word['karaoke_by_performer'] = 'по исполнителю';
+word['karaoke_by_title'] = 'по названию';
+word['karaoke_title'] = 'КАРАОКЕ';
+
+// layer
+word['layer_page'] = 'СТР';
+word['layer_from'] = 'ИЗ';
+word['layer_found'] = 'НАЙДЕНО';
+word['layer_records'] = 'ЗАПИСЕЙ';
+
+// media browser
+word['mbrowser_title'] = 'USB Устройство';
+word['mbrowser_connected'] = 'подключено';
+word['mbrowser_disconnected'] = 'отключено';
+word['mbrowser_not_found'] = 'не найдено';
+
+// player
+word['player_limit_notice'] = 'Количество подключений ограничено.<br>Попробуйте позже';
+word['player_file_missing'] = 'Файл отсутствует';
+word['player_server_error'] = 'Ошибка сервера';
+word['player_series'] = 'серия';
+word['player_series_uc'] = 'СЕРИЯ';
+word['player_track'] = 'Дорожка';
+word['player_off'] = 'Отключить';
+word['player_subtitle'] = 'Субтитры';
+word['player_claim'] = 'Пожаловаться';
+word['player_on_sound'] = 'на звук';
+word['player_on_video'] = 'на изображение';
+word['player_audio'] = 'Аудио';
+word['player_ty'] = 'Спасибо, Ваше мнение будет учтено';
+
+// radio
+word['radio_title'] = 'РАДИО';
+
+// tv
+word['tv_view'] = 'ОТОБРАЖЕНИЕ';
+word['tv_sort'] = 'СОРТИРОВКА';
+word['tv_favorite'] = 'ИЗБРАННОЕ';
+word['tv_move'] = 'ДВИГАТЬ';
+word['tv_by_number'] = 'по номеру';
+word['tv_by_title'] = 'по имени';
+word['tv_only_favorite'] = 'только избранное';
+word['tv_list'] = 'список';
+word['tv_list_w_info'] = 'список с инфо';
+word['tv_title'] = 'ТВ';
+
+// vclub info
+word['vclub_info'] = 'информация о фильме';
+word['vclub_year'] = 'Год';
+word['vclub_genre'] = 'Жанр';
+word['vclub_length'] = 'Продолжительность';
+word['vclub_minutes'] = 'мин';
+word['vclub_director'] = 'Режиссер';
+word['vclub_cast'] = 'В ролях';
+
+// vclub
+word['vclub_view'] = 'ОТОБРАЖЕНИЕ';
+word['vclub_sort'] = 'СОРТИРОВКА';
+word['vclub_search'] = 'ПОИСК';
+word['vclub_sampling'] = 'ВЫБОРКА';
+word['vclub_by_letter'] = 'ПО БУКВЕ';
+word['vclub_by_genre'] = 'ПО ЖАНРУ';
+word['vclub_by_year'] = 'ПО ГОДУ';
+word['vclub_by_title'] = 'по имени';
+word['vclub_by_addtime'] = 'по добавлению';
+word['vclub_top'] = 'по популярности';
+word['vclub_only_hd'] = 'только HD';
+word['vclub_only_favorite'] = 'только избранное';
+word['vclub_list'] = 'список';
+word['vclub_list_w_info'] = 'список с инфо';
+word['vclub_title'] = 'ВИДЕОКЛУБ';
+
+// common
+word['cut_off_msg'] = 'Ваша приставка отключена.<br/> т. 740-0-740';
+word['month_arr'] = ['ЯНВАРЯ', 'ФЕВРАЛЯ', 'МАРТА', 'АПРЕЛЯ', 'МАЯ', 'ИЮНЯ', 'ИЮЛЯ', 'АВГУСТА', 'СЕНТЯБРЯ', 'ОКТЯБРЯ', 'НОЯБРЯ', 'ДЕКАБРЯ'];
+word['week_arr'] = ['ВОСКРЕСЕНЬЕ', 'ПОНЕДЕЛЬНИК', 'ВТОРНИК', 'СРЕДА', 'ЧЕТВЕРГ', 'ПЯТНИЦА', 'СУББОТА'];
+word['year'] = 'г';
