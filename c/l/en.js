b87e754785ad5a62a9211a0f2031491c2730b126
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-08-19 09:22:46 +0000

    "Add en.js"

---
+++ c/l/en.js
@@ -0,0 +1,93 @@
+var word = [];
+
+// weather
+word['weather_comfort'] = 'Comfort';
+word['weather_pressure'] = 'Pressure';
+word['weather_mmhg'] = 'mm Hg';
+word['weather_wind'] = 'Wind';
+word['weather_speed'] = 'm/s';
+word['weather_humidity'] = 'Humidity';
+
+// karaoke
+word['karaoke_view'] = 'VIEW';
+word['karaoke_sort'] = 'SORT';
+word['karaoke_search'] = 'SEARCH';
+word['karaoke_sampling'] = 'PICKING';
+word['karaoke_by_letter'] = 'BY LETTER';
+word['karaoke_by_performer'] = 'BY NAME';
+word['karaoke_by_title'] = 'BY TITLE';
+word['karaoke_title'] = 'KARAOKE';
+
+// layer
+word['layer_page'] = 'PAGE';
+word['layer_from'] = 'FROM';
+word['layer_found'] = 'FOUND';
+word['layer_records'] = 'RECORDS';
+
+// media browser
+word['mbrowser_title'] = 'USB Device';
+word['mbrowser_connected'] = 'connected';
+word['mbrowser_disconnected'] = 'disconnected';
+word['mbrowser_not_found'] = 'not found';
+
+// player
+word['player_limit_notice'] = 'The number of connections is limited. <br> Try again later';
+word['player_file_missing'] = 'File missing';
+word['player_server_error'] = 'Server error';
+word['player_series'] = 'part';
+word['player_series_uc'] = 'PART';
+word['player_track'] = 'Track';
+word['player_off'] = 'Off';
+word['player_subtitle'] = 'Subtitles';
+word['player_claim'] = 'Complain';
+word['player_on_sound'] = 'on sound';
+word['player_on_video'] = 'on video';
+word['player_audio'] = 'Audio';
+word['player_ty'] = 'Thank you, your opinion will be taken into account';
+
+// radio
+word['radio_title'] = 'RADIO';
+
+// tv
+word['tv_view'] = 'VIEW';
+word['tv_sort'] = 'SORT';
+word['tv_favorite'] = 'FAVORITE';
+word['tv_move'] = 'MOVE';
+word['tv_by_number'] = 'by number';
+word['tv_by_title'] = 'by title';
+word['tv_only_favorite'] = 'only favorite';
+word['tv_list'] = 'list';
+word['tv_list_w_info'] = 'list + info';
+word['tv_title'] = 'TV';
+
+// vclub info
+word['vclub_info'] = 'information about the movie';
+word['vclub_year'] = 'Year';
+word['vclub_genre'] = 'Genre';
+word['vclub_length'] = 'Length';
+word['vclub_minutes'] = 'min';
+word['vclub_director'] = 'Director';
+word['vclub_cast'] = 'Cast';
+
+// vclub
+word['vclub_view'] = 'VIEW';
+word['vclub_sort'] = 'SORT';
+word['vclub_search'] = 'SEARCH';
+word['vclub_sampling'] = 'PICKING';
+word['vclub_by_letter'] = 'BY LETTER';
+word['vclub_by_genre'] = 'BY GENRE';
+word['vclub_by_year'] = 'BY YEAR';
+word['vclub_by_title'] = 'by title';
+word['vclub_by_addtime'] = 'by addtime';
+word['vclub_top'] = 'rating';
+word['vclub_only_hd'] = 'HD only';
+word['vclub_only_favorite'] = 'favorite only';
+word['vclub_list'] = 'list';
+word['vclub_list_w_info'] = 'list + info';
+word['vclub_title'] = 'VIDEOCLUB';
+
+// common
+word['cut_off_msg'] = 'Your STB is blocked.<br/> Call the provider';
+word['month_arr'] = ['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER'];
+word['week_arr'] = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+word['year'] = '';
