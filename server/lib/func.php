cabca086eb778b2497829806785b5e8bb3ef4f54
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-08-19 12:54:04 +0000

    "Comment debug"

--- server/lib/func.php
+++ server/lib/func.php
@@ -13,7 +13,7 @@ function get_save_folder($id){
     
     $dir_name = ceil($id/FILES_IN_DIR);
     $dir_path = IMG_PATH.$dir_name;
-    echo '$dir_path: '.$dir_path;
+    //echo '$dir_path: '.$dir_path;
     if (!is_dir($dir_path)){
         umask(0);
         if (!mkdir ($dir_path, 0777)){
