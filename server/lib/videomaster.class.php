0996d0ed00d7cbe4bd162a339e4f92d8c27b5c8a
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-08-31 07:58:15 +0000

    "Modify __construct, getMediaNameById, saveSeries"

--- server/lib/videomaster.class.php
+++ server/lib/videomaster.class.php
@@ -9,28 +9,27 @@
 class VideoMaster extends Master
 {
     public function __construct(){
-        parent::__construct();
+        
         $this->media_type = 'vclub';
+        $this->db_table = 'video';
+        
+        parent::__construct();
     }
     
-    protected function getMediaNameById($media_id){
+    protected function getMediaName(){
         
-        //$media_name = $this->db->executeQuery('select * from video where id='.$media_id)->getValueByName(0, 'path');
-        $media_name = $this->db->from('video')
-                               ->where(array('id' => $media_id))
-                               ->get()
-                               ->first('path');
-        
-        if ($media_name){
-            return $media_name;
-        }else{
-            return '';
+        if (!empty($this->media_params) && !empty($this->media_params['path'])){
+            
+            return $this->media_params['path'];
         }
+        
+        return '';
+        
     }
     
     protected function saveSeries($series_arr){
         sort($series_arr);
-        //$this->db->executeQuery("update video set series='".serialize($series_arr)."' where id='$this->media_id'");
+        
         $this->db->update('video',
                           array('series' => serialize($series_arr)),
                           array('id' => $this->media_id));
