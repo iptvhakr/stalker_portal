430cf3735753aa6513ee736aafcf8bedd73e711f
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-09-08 08:33:14 +0000

    "Add en.php"

---
+++ server/lang/en.php
@@ -0,0 +1,33 @@
+<?php
+
+$words = array();
+
+$words['all_abc'] = array('*','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','W','Z');
+
+$words['all_months'] = array(
+                    'january',
+                    'february',
+                    'march',
+                    'april',
+                    'may',
+                    'june',
+                    'july',
+                    'august',
+                    'september',
+                    'october',
+                    'november',
+                    'december'
+                );
+                
+$words['all_title'] = 'All';
+
+$words['no_ch_info'] = '[No channel info]';
+
+$words['vod_today'] = 'today';
+$words['vod_yesterday'] = 'yesterday';
+$words['vod_last_week'] = 'last week';
+
+$words['records_time_h'] = 'h';
+$words['records_time_m'] = 'm';
+$words['records_time_s'] = 's';
+?>
