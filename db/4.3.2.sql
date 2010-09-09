a9cfacc453b537029d41a76c33ce568d1634a6a0
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-09-09 10:46:44 +0000

    "Modify video, rec_files tables"

--- db/4.3.2.sql
+++ db/4.3.2.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `storages` ADD `for_moderator` tinyint default 0;
 ALTER TABLE `video` ADD `rtsp_url` varchar(255) NOT NULL default '';
+ALTER TABLE `rec_files` ADD `uid` int NOT NULL default 0;
