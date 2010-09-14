11297b746d3120ec9c9439ef8a72781c37dbdfa7
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-09-14 13:01:09 +0000

    "Add words from js"

--- server/lang/en.php
+++ server/lang/en.php
@@ -30,4 +30,105 @@ $words['vod_last_week'] = 'last week';
 $words['records_time_h'] = 'h';
 $words['records_time_m'] = 'm';
 $words['records_time_s'] = 's';
+
+// weather
+$words['weather_comfort'] = 'Comfort';
+$words['weather_pressure'] = 'Pressure';
+$words['weather_mmhg'] = 'mm Hg';
+$words['weather_wind'] = 'Wind';
+$words['weather_speed'] = 'm/s';
+$words['weather_humidity'] = 'Humidity';
+
+// karaoke
+$words['karaoke_view'] = 'VIEW';
+$words['karaoke_sort'] = 'SORT';
+$words['karaoke_search'] = 'SEARCH';
+$words['karaoke_sampling'] = 'PICKING';
+$words['karaoke_by_letter'] = 'BY LETTER';
+$words['karaoke_by_performer'] = 'BY NAME';
+$words['karaoke_by_title'] = 'BY TITLE';
+$words['karaoke_title'] = 'KARAOKE';
+
+// layer
+$words['layer_page'] = 'PAGE';
+$words['layer_from'] = 'FROM';
+$words['layer_found'] = 'FOUND';
+$words['layer_records'] = 'RECORDS';
+
+// media browser
+$words['mbrowser_title'] = 'USB Device';
+$words['mbrowser_connected'] = 'connected';
+$words['mbrowser_disconnected'] = 'disconnected';
+$words['mbrowser_not_found'] = 'not found';
+
+// player
+$words['player_limit_notice'] = 'The number of connections is limited. <br> Try again later';
+$words['player_file_missing'] = 'File missing';
+$words['player_server_error'] = 'Server error';
+$words['player_server_unavailable'] = 'Server unavailable';
+$words['player_series'] = 'part';
+$words['player_series_uc'] = 'PART';
+$words['player_track'] = 'Track';
+$words['player_off'] = 'Off';
+$words['player_subtitle'] = 'Subtitles';
+$words['player_claim'] = 'Complain';
+$words['player_on_sound'] = 'on sound';
+$words['player_on_video'] = 'on video';
+$words['player_audio'] = 'Audio';
+$words['player_ty'] = 'Thank you, your opinion will be taken into account';
+
+// radio
+$words['radio_title'] = 'RADIO';
+
+// tv
+$words['tv_view'] = 'VIEW';
+$words['tv_sort'] = 'SORT';
+$words['tv_favorite'] = 'FAVORITE';
+$words['tv_move'] = 'MOVE';
+$words['tv_by_number'] = 'by number';
+$words['tv_by_title'] = 'by title';
+$words['tv_only_favorite'] = 'only favorite';
+$words['tv_list'] = 'list';
+$words['tv_list_w_info'] = 'list + info';
+$words['tv_title'] = 'TV';
+
+// vclub info
+$words['vclub_info'] = 'information about the movie';
+$words['vclub_year'] = 'Year';
+$words['vclub_genre'] = 'Genre';
+$words['vclub_length'] = 'Length';
+$words['vclub_minutes'] = 'min';
+$words['vclub_director'] = 'Director';
+$words['vclub_cast'] = 'Cast';
+
+// vclub
+$words['vclub_view'] = 'VIEW';
+$words['vclub_sort'] = 'SORT';
+$words['vclub_search'] = 'SEARCH';
+$words['vclub_sampling'] = 'PICKING';
+$words['vclub_by_letter'] = 'BY LETTER';
+$words['vclub_by_genre'] = 'BY GENRE';
+$words['vclub_by_year'] = 'BY YEAR';
+$words['vclub_by_title'] = 'by title';
+$words['vclub_by_addtime'] = 'by addtime';
+$words['vclub_top'] = 'rating';
+$words['vclub_only_hd'] = 'HD only';
+$words['vclub_only_favorite'] = 'favorite only';
+$words['vclub_list'] = 'list';
+$words['vclub_list_w_info'] = 'list + info';
+$words['vclub_title'] = 'VIDEOCLUB';
+
+// common
+$words['cut_off_msg'] = 'Your STB is blocked.<br/> Call the provider';
+$words['month_arr'] = array('JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER');
+$words['week_arr'] = array('SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY');
+$words['year'] = '';
+
+// pvr
+$words['records_title'] = 'RECORDS';
+
+// ears
+$words['ears_back'] = 'B<br>A<br>C<br>K';
+$words['ears_about_movie'] = 'A<br>B<br>O<br>U<br>T<br><br>M<br>O<br>V<br>I<br>E';
+$words['ears_tv_guide'] = 'T<br>V<br><br>G<br>U<br>I<br>D<br>E';
 ?>
