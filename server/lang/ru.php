88334c3e5181132d619b1b7f1883eb551fd8af3a
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-09-14 13:01:11 +0000

    "Add words from js"

--- server/lang/ru.php
+++ server/lang/ru.php
@@ -30,4 +30,105 @@ $words['vod_last_week'] = 'последние 7 дней';
 $words['records_time_h'] = 'ч';
 $words['records_time_m'] = 'м';
 $words['records_time_s'] = 'с';
+
+// weather
+$words['weather_comfort'] = 'Комфорт';
+$words['weather_pressure'] = 'Давление';
+$words['weather_mmhg'] = 'мм. рт.ст.';
+$words['weather_wind'] = 'Ветер';
+$words['weather_speed'] = 'м/с';
+$words['weather_humidity'] = 'Влажность';
+
+// karaoke
+$words['karaoke_view'] = 'ОТОБРАЖЕНИЕ';
+$words['karaoke_sort'] = 'СОРТИРОВКА';
+$words['karaoke_search'] = 'ПОИСК';
+$words['karaoke_sampling'] = 'ВЫБОРКА';
+$words['karaoke_by_letter'] = 'ПО БУКВЕ';
+$words['karaoke_by_performer'] = 'по исполнителю';
+$words['karaoke_by_title'] = 'по названию';
+$words['karaoke_title'] = 'КАРАОКЕ';
+
+// layer
+$words['layer_page'] = 'СТР';
+$words['layer_from'] = 'ИЗ';
+$words['layer_found'] = 'НАЙДЕНО';
+$words['layer_records'] = 'ЗАПИСЕЙ';
+
+// media browser
+$words['mbrowser_title'] = 'USB Устройство';
+$words['mbrowser_connected'] = 'подключено';
+$words['mbrowser_disconnected'] = 'отключено';
+$words['mbrowser_not_found'] = 'не найдено';
+
+// player
+$words['player_limit_notice'] = 'Количество подключений ограничено.<br>Попробуйте позже';
+$words['player_file_missing'] = 'Файл отсутствует';
+$words['player_server_error'] = 'Ошибка сервера';
+$words['player_server_unavailable'] = 'Сервер недоступен';
+$words['player_series'] = 'серия';
+$words['player_series_uc'] = 'СЕРИЯ';
+$words['player_track'] = 'Дорожка';
+$words['player_off'] = 'Отключить';
+$words['player_subtitle'] = 'Субтитры';
+$words['player_claim'] = 'Пожаловаться';
+$words['player_on_sound'] = 'на звук';
+$words['player_on_video'] = 'на изображение';
+$words['player_audio'] = 'Аудио';
+$words['player_ty'] = 'Спасибо, Ваше мнение будет учтено';
+
+// radio
+$words['radio_title'] = 'РАДИО';
+
+// tv
+$words['tv_view'] = 'ОТОБРАЖЕНИЕ';
+$words['tv_sort'] = 'СОРТИРОВКА';
+$words['tv_favorite'] = 'ИЗБРАННОЕ';
+$words['tv_move'] = 'ДВИГАТЬ';
+$words['tv_by_number'] = 'по номеру';
+$words['tv_by_title'] = 'по имени';
+$words['tv_only_favorite'] = 'только избранное';
+$words['tv_list'] = 'список';
+$words['tv_list_w_info'] = 'список с инфо';
+$words['tv_title'] = 'ТВ';
+
+// vclub info
+$words['vclub_info'] = 'информация о фильме';
+$words['vclub_year'] = 'Год';
+$words['vclub_genre'] = 'Жанр';
+$words['vclub_length'] = 'Продолжительность';
+$words['vclub_minutes'] = 'мин';
+$words['vclub_director'] = 'Режиссер';
+$words['vclub_cast'] = 'В ролях';
+
+// vclub
+$words['vclub_view'] = 'ОТОБРАЖЕНИЕ';
+$words['vclub_sort'] = 'СОРТИРОВКА';
+$words['vclub_search'] = 'ПОИСК';
+$words['vclub_sampling'] = 'ВЫБОРКА';
+$words['vclub_by_letter'] = 'ПО БУКВЕ';
+$words['vclub_by_genre'] = 'ПО ЖАНРУ';
+$words['vclub_by_year'] = 'ПО ГОДУ';
+$words['vclub_by_title'] = 'по имени';
+$words['vclub_by_addtime'] = 'по добавлению';
+$words['vclub_top'] = 'по популярности';
+$words['vclub_only_hd'] = 'только HD';
+$words['vclub_only_favorite'] = 'только избранное';
+$words['vclub_list'] = 'список';
+$words['vclub_list_w_info'] = 'список с инфо';
+$words['vclub_title'] = 'ВИДЕОКЛУБ';
+
+// common
+$words['cut_off_msg'] = 'Ваша приставка отключена.<br/> т. 740-0-740';
+$words['month_arr'] = array('ЯНВАРЯ', 'ФЕВРАЛЯ', 'МАРТА', 'АПРЕЛЯ', 'МАЯ', 'ИЮНЯ', 'ИЮЛЯ', 'АВГУСТА', 'СЕНТЯБРЯ', 'ОКТЯБРЯ', 'НОЯБРЯ', 'ДЕКАБРЯ');
+$words['week_arr'] = array('ВОСКРЕСЕНЬЕ', 'ПОНЕДЕЛЬНИК', 'ВТОРНИК', 'СРЕДА', 'ЧЕТВЕРГ', 'ПЯТНИЦА', 'СУББОТА');
+$words['year'] = 'г';
+
+// pvr
+$words['records_title'] = 'ЗАПИСИ';
+
+// ears
+$words['ears_back'] = 'Н<br>А<br>З<br>А<br>Д';
+$words['ears_about_movie'] = 'О<br><br>Ф<br>И<br>Л<br>Ь<br>М<br>Е';
+$words['ears_tv_guide'] = 'Т<br>В<br><br>Г<br>И<br>Д';
 ?>
