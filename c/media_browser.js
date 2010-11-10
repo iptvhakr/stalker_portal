c244f39bd3bcec6dba5df2b72838bddef75041f8
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-11-10 14:35:42 +0000

    "Add module to main_menu.add"

--- c/media_browser.js
+++ c/media_browser.js
@@ -281,7 +281,9 @@
         }else{
             stb.notice.show(word['mbrowser_title'] + ' ' + word['mbrowser_not_found']);
         }
-    });
+    },
+    module.media_browser
+    );
     
 })();
 
