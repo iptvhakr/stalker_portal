c07b2f5ac50ac6083138b8bad5a4ced41e528bd3
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-11-18 14:14:23 +0000

    "Remove Database"

--- server/load.php
+++ server/load.php
@@ -26,21 +26,21 @@ if (isset($_GET['JsHttpRequest'])){
     $JsHttpRequest = new JsHttpRequest("utf-8");
     $loader = new DataLoader($_REQUEST['type'], $_REQUEST['action']);
     $GLOBALS['_RESULT'] = $loader->getResult();
-    $mysql = Mysql::getInstance();
-    $counter = $mysql->get_num_queries();
+    //$mysql = Mysql::getInstance();
+    //$counter = $mysql->get_num_queries();
 }else{
     $JsHttpRequest = new Subsys_JsHttpRequest_Php("utf-8");
     $_RESULT = get_data();
     $counter = 0;
 }
 
-$db = Database::getInstance(DB_NAME);
+//$db = Database::getInstance(DB_NAME);
 
 //$log = Log::getInstance();
 
-$generated_in = round(microtime(1) - $start_time, 3);
+//$generated_in = round(microtime(1) - $start_time, 3);
 
 //$log->savePageGenerationTime($generated_in);
 
-echo "generated in: ".$generated_in."s; query counter: ".($db->query_counter+$counter)."; ".$debug->getErrorStr();
+echo "generated in: ".round(microtime(1) - $start_time, 3)."s; query counter: ".Mysql::get_num_queries()."; ".$debug->getErrorStr();
 ?>
