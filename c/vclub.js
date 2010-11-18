fc76187a358b7a130e8fe5517888d26f530df2a0
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-11-18 10:51:08 +0000

    "Modify set_not_ended"

--- c/vclub.js
+++ c/vclub.js
@@ -411,7 +411,7 @@ _debug('1!!!!!!!!!!!!!!');
         this.set_not_ended = function(video_id, series, end_time){
             _debug('vclub.set_not_ended', video_id, series, end_time);
             
-            if (empty(this.data_items[this.cur_row].series)){
+            if (this.load_params.not_ended && empty(this.data_items[this.cur_row].series)){
                 this.data_items[this.cur_row].position = end_time;
             }
             
