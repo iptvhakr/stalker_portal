9f7087368e1237825be914420e78ff4611ce2afb
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-11-29 10:10:27 +0000

    "Move up include conf_serv.php"

--- server/api/testers_macs.php
+++ server/api/testers_macs.php
@@ -1,7 +1,7 @@
 <?php
 
-include "../common.php";
 include "../conf_serv.php";
+include "../common.php";
 
 echo 'api_callback('.json_encode(Mysql::getInstance()->from('testers')->where(array('status' => 1))->get()->all('mac')).')';
 
