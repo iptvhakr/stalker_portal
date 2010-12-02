af46ceb4da8b34bbe9cfc2c94c9eb5b4f56df77e
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-12-02 12:56:31 +0000

    "Add volume correction fields"

--- db/4.4.5.sql
+++ db/4.4.5.sql
@@ -1,2 +1,4 @@
 ALTER TABLE moderators_history ADD INDEX msqs (`task_id`, `to_usr`, `readed`);
 ALTER TABLE moderator_tasks ADD INDEX to_user (`ended`, `archived`, `to_usr`);
+ALTER TABLE itv ADD `volume_correction` int NOT NULL default 0;
+ALTER TABLE video ADD `volume_correction` int NOT NULL default 0;
