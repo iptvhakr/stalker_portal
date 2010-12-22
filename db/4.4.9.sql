e4a3fe1e35fdc00d4a6537189db07e7d8db96d79
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2010-12-22 15:33:03 +0000

    "Modify fav_vclub status"

---
+++ db/4.4.9.sql
@@ -0,0 +1,5 @@
+CREATE TEMPORARY TABLE `tmp_fav_vclub` AS SELECT * FROM `fav_vclub` GROUP BY `uid`;
+DELETE FROM `fav_vclub`;
+ALTER TABLE `fav_vclub` ADD UNIQUE INDEX (`uid`);
+INSERT INTO `fav_vclub` SELECT * FROM `tmp_fav_vclub`;
+DROP TABLE `tmp_fav_vclub`;
