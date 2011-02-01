215097305d87126a4e19c8b701c785095c647796
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-02-01 13:10:33 +0000

    "Add tv_reminder table"

--- db/4.4.9.sql
+++ db/4.4.9.sql
@@ -4,4 +4,13 @@ ALTER TABLE `fav_vclub` ADD UNIQUE INDEX (`uid`);
 INSERT INTO `fav_vclub` SELECT * FROM `tmp_fav_vclub`;
 DROP TABLE `tmp_fav_vclub`;
 
-ALTER TABLE `tv_reminder` ADD INDEX `tv_program_id` (`tv_program_id`);
+CREATE TABLE `tv_reminder`(
+    `id` int NOT NULL auto_increment,
+    `mac` varchar(64) NOT NULL default '',
+    `ch_id` int NOT NULL default 0,
+    `tv_program_id` int NOT NULL default 0,
+    `fire_time` datetime,
+    `added` datetime,
+    PRIMARY KEY (`id`),
+    INDEX `tv_program_id` (`tv_program_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
