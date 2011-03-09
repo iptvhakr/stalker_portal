2ffd52e6f4a562809ebb116266cddee4128b5904
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-03-09 09:40:23 +0000

    Add tmp link checker

---
+++ server/api/chk_tmp_tv_link.php
@@ -0,0 +1,14 @@
+<?php
+
+include "../conf_serv.php";
+include "../common.php";
+
+$result = Itv::checkTemporaryLink($_GET['key']);
+
+if (!$result){
+    $result = '/404/';
+}
+
+header("X-Accel-Redirect: ".$result);
+
+?>
