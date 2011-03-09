2148a1ed9c444333e4c5f4fea315a9e78e72c5c1
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-03-09 09:41:58 +0000

    Modify get and set

--- server/lib/cache.class.php
+++ server/lib/cache.class.php
@@ -11,6 +11,7 @@ class Cache
 {
     
     private $backend;
+    private $use_custom_caching = false;
     
     private static $instance = NULL;
     
@@ -28,38 +29,66 @@ class Cache
         
         $this->backend->connect(MEMCACHE_HOST, 11211);
     }
-      
-    public function get($key){
-        
+
+    public function useCustomCaching($val){
+        $this->use_custom_caching = !!$val;
+    }
+
+    private function customGet($key){
         $val = $this->backend->get($key);
-        
+
         if (!is_array($val)){
             return $val;
         }
-        
+
         if(floatval($val['expire']) < time() && $val['expire'] != 0){
             return false;
         }
-        
+
         if ($this->isInvalidTags($val['tags'])){
             return false;
         }
-        
+
         return $val['data'];
-        
     }
-    
-    public function set($key, $val, $tags, $expire = 0){
+
+    private function realGet($key){
+        return $this->backend->get($key);
+    }
+      
+    public function get($key){
         
+        if ($this->use_custom_caching){
+            return $this->customGet($key);
+        }else{
+            return $this->realGet($key);
+        }
+    }
+
+    private function customSet($key, $val, $tags, $expire = 0){
+
         $tags = $this->setInvalidTags($tags);
-        
+
         $prepared_val = array(
             'expire' => ($expire == 0)? 0 : $expire + time(),
             'data'   => $val,
             'tags'   => $tags
         );
-        
+
         return $this->backend->set($key, $prepared_val, 0, 0);
+    }
+
+    private function realSet($key, $val, $flag = 0, $expire = 0){
+        return $this->backend->set($key, $val, $flag, $expire);
+    }
+    
+    public function set($key, $val, $tags = array(), $expire = 0){
+        
+        if ($this->use_custom_caching){
+            return $this->customSet($key, $val, $tags, $expire);
+        }else{
+            return $this->realSet($key, $val, 0, $expire);
+        }
     }
 
     public function setInvalidTags($tags){
