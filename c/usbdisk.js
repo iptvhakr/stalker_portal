6eb85d8a7a8eea1e368e33b297bbc25980dcefe0
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-03-15 16:40:11 +0000

    Modify is_drive_mounted

--- c/usbdisk.js
+++ c/usbdisk.js
@@ -16,22 +16,6 @@ function usbdisk(){
     }catch(e){
         _debug(e);
     }
-    
-    /*var self = this;
-            
-    (function(){
-        try{
-            self.drive_mounted();
-        }catch(e){
-            _debug(e);
-        }
-    }).bind(key.USB_MOUNTED);
-    
-    (function(){
-        self.drive_umounted();
-    }).bind(key.USB_UNMOUNTED);
-
-    this.check_mounted();*/
 }
 
 usbdisk.prototype.init = function(){
@@ -52,39 +36,35 @@ usbdisk.prototype.init = function(){
     }).bind(key.USB_UNMOUNTED);
 
     this.check_mounted();
-}
+};
 
 usbdisk.prototype.drive_mounted = function(){
     _debug('usbdisk.drive_mounted');
     
     this.mounted = true;
     
-    stb.notice.show(get_word('mbrowser_title') + ' ' + get_word('mbrowser_connected'));
+    stb.notice.show(get_word('usb_drive') + ' ' + get_word('mbrowser_connected'));
     
     var self = this;
     
     window.setTimeout(function(){self.fire_onmount_callbacks()}, 100);
-}
+};
 
 usbdisk.prototype.drive_umounted = function(){
     _debug('usbdisk.drive_umounted');
     
     this.mounted = false;
     
-    stb.notice.show(get_word('mbrowser_title') + ' ' + get_word('mbrowser_disconnected'));
+    stb.notice.show(get_word('usb_drive') + ' ' + get_word('mbrowser_disconnected'));
     
     this.fire_onumount_callbacks();
-}
+};
 
 usbdisk.prototype.is_drive_mounted = function(){
     _debug('usbdisk.is_drive_mounted');
     
-    if (this.mounted){
-        return true;
-    }
-    
-    return false;
-}
+    return this.mounted;
+};
 
 usbdisk.prototype.check_mounted = function(){
     _debug('usbdisk.check_mounted');
@@ -102,9 +82,7 @@ usbdisk.prototype.check_mounted = function(){
     }catch(e){
         _debug(e);
     }
-    
-    return;  
-}
+};
 
 usbdisk.prototype.read_dir = function(path){
     _debug('usbdisk.read_dir');
@@ -124,27 +102,27 @@ usbdisk.prototype.read_dir = function(path){
     }catch(e){
         _debug(e);
     }
-}
+};
 
 usbdisk.prototype.add_onmount_callback = function(func){
     _debug('usbdisk.add_onmount_callback');
     
     this.onmount_callbacks.push(func);
-}
+};
 
 usbdisk.prototype.add_onumount_callback = function(func){
     _debug('usbdisk.add_onumount_callback');
     
     this.onumount_callbacks.push(func);
-}
+};
 
 usbdisk.prototype.fire_onmount_callbacks = function(){
-    _debug('usbdisk.fire_onmount_callbacks')
+    _debug('usbdisk.fire_onmount_callbacks');
     
     for(var i=0; i<this.onmount_callbacks.length; i++){
         this.onmount_callbacks[i]();
     }
-}
+};
 
 usbdisk.prototype.fire_onumount_callbacks = function(){
     _debug('usbdisk.fire_onumount_callbacks');
@@ -152,4 +130,4 @@ usbdisk.prototype.fire_onumount_callbacks = function(){
     for(var i=0; i<this.onumount_callbacks.length; i++){
         this.onumount_callbacks[i]();
     }
-}
+};
