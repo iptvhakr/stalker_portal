b38e56f853d270e9cd56a881484a1cbc4c67f6fe
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-03-25 13:13:14 +0000

    Add project_path constant

--- server/common.php
+++ server/common.php
@@ -12,7 +12,9 @@ if (!defined("PATH_SEPARATOR")){
     define("PATH_SEPARATOR", getenv("COMSPEC")? ";" : ":");
 }
 
-ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.dirname(__FILE__));
+define('PROJECT_PATH', dirname(__FILE__));
+
+ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.PROJECT_PATH);
 
 function __autoload($class_name) {
     $class = 'lib/'.strtolower($class_name).'.class.php';
