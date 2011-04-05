7c9f90b87803b7552535d2e99d6a24f285b132e6
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-05 10:03:48 +0000

    Add misctranslations.php

---
+++ server/tools/misctranslations.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ *  Auxiliary file to translation data from a database using gettext
+ */
+
+/**
+ * cat_genre table
+ */
+_('biography');
+_('action');
+_('western');
+_('military');
+_('detective');
+_("children's");
+_('house/country');
+_('drama');
+_('foreign');
+_('health');
+_('art');
+_('historical');
+_('history');
+_('yoga');
+_('catastrophe');
+_('comedy');
+_('criminal');
+_('cookery');
+_('melodrama');
+_('mysticism');
+_('musical');
+_('science');
+_('ours');
+_('teach');
+_('hunting');
+_('adventure');
+_('nature');
+_('travels');
+_('fishing');
+_('series');
+_('sketch-show');
+_('sport');
+_('dancing');
+_('show');
+_('technique');
+_('thriller');
+_('horror');
+_('fiction');
+_('aerobics');
+_('fantasy');
+_('erotica');
+_('humourist');
+
+/**
+ * media_category table
+ */
+_('Cartoon');
+_('Ours cinema');
+_('World cinema');
+_('Documentary');
+_('Humour');
+_('Hobbies');
+_('Our Serial');
+_('Foreign Serial');
+
+/**
+ * genre table
+ */
+_('Action');
+_('Detective');
+_('Documentary');
+_('Drama');
+_('Historical');
+_('Clips');
+_('Comedy');
+_('Melodrama');
+_('Humour');
+_('Adventures');
+_('Thriller');
+_('Horrors');
+_('Fiction');
+_('Erotica');
+_('Fantasy');
+_('Animation');
+_('Childrens');
+_('Musical');
+_('Western');
+_('Serial');
+_('Sports');
+_('Teach');
+_('Information');
+
+/**
+ * tv_genre table
+ */
+_('information');
+_('entertainments');
+_("children's");
+_('cinema');
+_('science');
+_('sports');
+_('music');
+_('business');
+_('culture');
+_('for adults');
+
+/**
+ * karaoke_genre table
+ */
+_('Pop');
+_('Rock');
+_('Chanson');
+_('From cartoon films');
+_('Nurseries');
+_('National');
+_('Jazz');
+_('From films');
+
+/**
+ * weatherco_cache table and weatherco.class.php
+ */
+_('Clear');
+_('Partly Cloudy');
+_('Cloudy');
+_('Overcast');
+_('Little rain');
+_('Rain');
+_('Thunderstorm');
+_('Hail');
+_('Sleet');
+_('Snow');
+_('Snowfall');
+
+/// North
+_('N');
+/// North-East
+_('NE');
+/// East
+_('E');
+/// South-East
+_('SE');
+/// South
+_('S');
+/// South-West
+_('SW');
+/// West
+_('W');
+/// North-West
+_('NW');
+
+?>
