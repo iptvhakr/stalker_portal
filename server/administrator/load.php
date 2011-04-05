613f61f812acc56b265ddc120968793821e8b67a
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-05 11:00:51 +0000

    Add gettext support.

--- server/administrator/load.php
+++ server/administrator/load.php
@@ -9,6 +9,15 @@ require_once "./lib/data.php";
 include "../common.php";
 require_once "../lib/func.php";
 
+$locale = 'ru_RU.utf8';
+
+setlocale(LC_MESSAGES, $locale);
+putenv('LC_MESSAGES='.$locale);
+
+bindtextdomain('stb', PROJECT_PATH.'/locale');
+textdomain('stb');
+bind_textdomain_codeset('stb', 'UTF-8');
+
 $JsHttpRequest = new Subsys_JsHttpRequest_Php("utf-8");
 
 // Формируем результат прямо в виде PHP-массива!
