0fe651b68d47f4563a6bf70f25675aa00c21fec7
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-05 10:18:25 +0000

    Changed OptionInput

--- c/layer.input.js
+++ c/layer.input.js
@@ -2,7 +2,6 @@
  * Base input constructor.
  * @constructor
  */
-
 function BaseInput(){
     
     this.label = '';
@@ -24,7 +23,6 @@ function BaseInput(){
  * Text input constructor.
  * @constructor
  */
-
 function TextInput(parent, options){
     
     this.type = 'text';
@@ -69,32 +67,50 @@ TextInput.prototype.build = function(){
     dl.appendChild(dd);
     
     this.parent.container.appendChild(dl);
-}
+};
 
 TextInput.prototype.add = function(symbol){
     _debug('TextInput.add', symbol);
     
     this.input.value = this.input.value + '' + symbol;
-}
+};
 
 TextInput.prototype.del = function(){
     _debug('TextInput.del');
     
     this.input.value = this.input.value.substr(0 ,this.input.value.length-1)
-}
+};
 
 TextInput.prototype.reset = function(){
-    _debug('TextInput.reset')
+    _debug('TextInput.reset');
     
     this.input.value = '';
-}
+};
+
+TextInput.prototype.set_active = function(){
+    _debug('TextInput.set_active');
+
+    this.input.setClass('active_input');
+};
+
+TextInput.prototype.set_passive = function(){
+    _debug('TextInput.set_passive');
+
+    this.input.setClass('passive_input');
+};
+
+TextInput.prototype.get_value = function(){
+    _debug('TextInput.get_value');
+
+    return this.input.value;
+};
+
 /* END Text input */
 
 /**
  * Option input constructor.
  * @constructor
  */
-
 function OptionInput(parent, options){
     
     this.parent = parent;
@@ -120,13 +136,17 @@ function OptionInput(parent, options){
             }else{
                 this.parent.set_passive();
             }
-        },
-    }
+        }
+    };
     
     this.map = this.map || [];
-    
-    this.default_checked_idx = 0;
-    this.checked_idx = 0;
+
+    this.map.options = this.map.options || [];
+
+    this.onchange = function(){};
+    this.onselect = function(){};
+
+    this.default_checked_idx = -1;
     this.cur_idx = 0;
     
     this.build();
@@ -136,130 +156,185 @@ OptionInput.prototype = new BaseInput();
 
 OptionInput.prototype.build = function(){
     _debug('OptionInput.build');
-    
-    var dl = document.createElement('dl');
-    
-    var dt = document.createElement('dt');
-    dt.innerHTML = this.label;
-    
-    var dd = document.createElement('dd');
-    
-    /*this.input = document.createElement('input');
-    this.input.type  = this.type;
-    this.input.value = this.default_val;
-    this.input.disabled = "disabled";
-    
-    dd.appendChild(this.input);*/
-    
-    if (this.map && this.map.options){
-        
-        for (var i=0; i<this.map.options.length; i++){
-            
-            var opt_dl = document.createElement('dl');
-            if(i == 0){
-                opt_dl.style.clear = 'none';
-            }
-            
-            var opt_dt = document.createElement('dt');
-            
-            var btn = this.map.options[i].border = create_block_element('passive_input', opt_dt);
-            btn.id = 'radio_btn';
-            
-            var bullet = this.map.options[i].bullet = create_block_element('checked', btn);
-            
-            if(!this.map.options[i].selected){
-                bullet.hide();
-            }else{
-                this.default_checked_idx = this.checked_idx = i;
-                this.input.value = this.map.options[i].value || this.map.options[i].label;
-            }
-            
-            var opt_dd = document.createElement('dt');
-            
-            opt_dd.innerHTML = this.map.options[i].label;
-            
-            opt_dl.appendChild(opt_dt);
-            opt_dl.appendChild(opt_dd);
-            dd.appendChild(opt_dl);
-        }
-    }
-    
-    dl.appendChild(dt);
-    dl.appendChild(dd);
-    
-    this.parent.container.appendChild(dl);
-}
 
-OptionInput.prototype.set_passive = function(){
-    _debug('OptionInput.set_passive', this.cur_idx);
-    
-    this.map.options[this.cur_idx].border.setClass('passive_input');
-}
+    var container = create_block_element('option_container', this.parent.container);
+
+    var label = create_block_element('option_label', container);
+    label.innerHTML = this.label;
+
+    this.larr = create_block_element('larr', container);
+    this.larr.innerHTML = '&lsaquo;';
+
+    this.option = create_block_element('option_value', container);
+
+    this.rarr = create_block_element('rarr', container);
+    this.rarr.innerHTML = '&rsaquo;';
+
+    var idx = this.map.options.getIdxByVal('selected', 1);
+
+    if (idx !== null){
+        this.default_checked_idx = this.cur_idx = idx;
+    }
+
+    this._set_option(this.cur_idx);
+};
 
 OptionInput.prototype.set_active = function(){
+    _debug('OptionInput.set_passive', this.cur_idx);
+
+    this.option.setAttribute('active', 'active');
+};
+
+OptionInput.prototype.set_passive = function(){
     _debug('OptionInput.set_active', this.cur_idx);
-    
-    this.map.options[this.cur_idx].border.setClass('active_input');
-}
 
-OptionInput.prototype.set_unchecked = function(){
-    _debug('OptionInput.set_unchecked', this.checked_idx);
-    
-    this.map.options[this.checked_idx].bullet.hide();
-}
-
-OptionInput.prototype.set_checked = function(){
-    _debug('OptionInput.set_checked');
-    
-    this.set_unchecked();
-    
-    this.checked_idx = this.cur_idx;
-    
-    this.input.value = this.map.options[this.checked_idx].value || this.map.options[this.checked_idx].label;
-    
-    this.map.options[this.checked_idx].bullet.show();
-}
+    this.option.setAttribute('active', '');
+};
 
 OptionInput.prototype.set_default = function(){
     _debug('OptionInput.set_default');
-    
-    this.set_passive();
-    
+
+    if (this.default_checked_idx < 0){
+        return;
+    }
+
     this.cur_idx = this.default_checked_idx;
+
+    this._set_option(this.cur_idx);
+};
+
+OptionInput.prototype.fill = function(data){
+    _debug('OptionInput.fill', data);
     
-    this.set_checked();
-    
-    this.set_active();
-}
+    this.map = data;
+
+    var idx = this.map.options.getIdxByVal('selected', 1);
+
+    if (idx !== null){
+        this.default_checked_idx = this.cur_idx = idx;
+    }else{
+        this.default_checked_idx = -1;
+        this.cur_idx = 0;
+    }
+
+    this._set_option(this.cur_idx);
+};
+
+OptionInput.prototype._set_option = function(idx){
+    _debug('OptionInput._set_option', idx);
+
+    this.cur_idx = idx;
+
+    window.clearTimeout(this.onchange_timer);
+
+    var self = this;
+
+    this.onchange_timer = window.setTimeout(function(){self.onchange()}, 500);
+
+    _debug('this.cur_idx', this.cur_idx);
+
+    if (this.map.options[this.cur_idx]){
+        this.option.innerHTML = this.map.options[this.cur_idx].label || this.map.options[this.cur_idx].value || '...';
+    }else{
+        this.option.innerHTML = '...';
+    }
+
+    if (this.cur_idx == this.map.options.length - 1 || this.map.options.length == 0){
+        this.rarr.style.visibility = 'hidden';
+    }else{
+        this.rarr.style.visibility = 'visible';
+    }
+
+    if (this.cur_idx == 0){
+        this.larr.style.visibility = 'hidden';
+    }else{
+        this.larr.style.visibility = 'visible';
+    }
+};
+
+OptionInput.prototype.get_value = function(){
+    _debug('OptionInput.get_value');
+
+    return this.map.options[this.cur_idx].value;
+};
+
+OptionInput.prototype.get_selected = function(){
+    _debug('OptionInput.get_value');
+
+    return this.map.options[this.cur_idx];
+};
+
+OptionInput.prototype.set_selected_by_value = function(value){
+    _debug('OptionInput.set_selected_by_value');
+
+    //if (this.default_checked_idx >= 0){
+    //    return;
+    //}
+
+    var idx = this.map.options.getIdxByVal('value', value);
+
+    if (idx !== null){
+        return this._set_option(idx);
+    }
+
+    return false;
+};
 
 OptionInput.prototype.shift = function(dir){
     _debug('OptionInput.shift', dir);
-    
+
     _debug('this.cur_idx', this.cur_idx);
     _debug('this.map.options.length', this.map.options.length);
-    
+
+    window.clearTimeout(this.onselect_timer);
+    var self = this;
+    this.onselect_timer = window.setTimeout(function(){self.onselect()}, 500);
+
     if (dir > 0){
-        
+
         if (this.cur_idx < this.map.options.length - 1){
-            this.set_passive();
             this.cur_idx++;
-            this.set_active();
         }else{
-            
             return false;
         }
     }else{
-        
+
         if (this.cur_idx > 0){
-            this.set_passive();
             this.cur_idx--;
-            this.set_active();
         }else{
             return false;
         }
     }
-    
+
+    this._set_option(this.cur_idx);
+
     return true;
-}
+};
+
+OptionInput.prototype.shift_page = function(dir){
+    _debug('OptionInput.shift_page', dir);
+
+    window.clearTimeout(this.onselect_timer);
+    var self = this;
+    this.onselect_timer = window.setTimeout(function(){self.onselect()}, 500);
+
+    if (dir > 0){
+        if (this.cur_idx < this.map.options.length - 11){
+            this.cur_idx = this.cur_idx + 10;
+        }else{
+            this.cur_idx = this.map.options.length - 1;
+        }
+    }else{
+        if (this.cur_idx > 10){
+            this.cur_idx = this.cur_idx - 10;
+        }else{
+            this.cur_idx = 0;
+        }
+    }
+
+    this._set_option(this.cur_idx);
+
+    return true;
+};
 
 loader.next();
