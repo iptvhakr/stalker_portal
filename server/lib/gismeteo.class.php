4989803af570f98dcdf74c23ba8ef6e88803bb68
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-05 11:04:20 +0000

    Add gettext localization for some words.

--- server/lib/gismeteo.class.php
+++ server/lib/gismeteo.class.php
@@ -12,18 +12,25 @@ class Gismeteo
     private $cache_table = 'gismeteo_day_weather';
     //private $cache_expire = 3600;
     public  $xml_url = GISMETEO_XML;
-    private $weekday_arr = array('','Вс','Пн','Вт','Ср','Чт','Пт','Сб');
-    private $tod_arr = array('Ночь','Утро','День','Вечер');
-    private $month_arr = array('','янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек');
-    private $cloudiness_arr = array('ясно','малооблачно','облачно','пасмурно');
-    private $precipitation_arr = array('','','','','дождь','ливень',' снег',' снег','гроза','нет данных','без осадков');
-    private $direction_arr = array('С','СВ','В','ЮВ','Ю','ЮЗ','З','СЗ');
+    private $weekday_arr = array();
+    private $tod_arr = array();
+    private $month_arr = array();
+    private $cloudiness_arr = array();
+    private $precipitation_arr = array();
+    private $direction_arr = array();
     private $precipitation_img_arr = array('','','','','w_rain.png','w_rain_strong.png','w_snow.png','w_snow.png','w_thunderstorm.png','','w_empty.png');
     private $cloudiness_img_arr = array('w_empty.png','w_cloud_small.png','w_cloud_big.png','w_cloud_black.png');
     
     public function __construct(){
         $this->db = Mysql::getInstance();
         $this->xml_url = $this->xml_url.'?'.time();
+
+        $this->weekday_arr = array('',_('Sun'),_('Mon'),_('Tue'),_('Wed'),_('Thu'),_('Fri'),('Sat'));
+        $this->tod_arr = array(_('Night'),_('Morning'),_('Day'),_('Evening'));
+        $this->month_arr = array('',_('Jan'),_('Feb'),_('Mar'),_('Apr'),_('May'),_('Jun'),_('Jul'),_('Aug'),_('Sep'),_('Oct'),_('Nov'),_('Dec'));
+        $this->cloudiness_arr = array(_('clear'),_('partly cloudy'),_('cloudy'),_('overcast'));
+        $this->precipitation_arr = array('','','','',_('rain'),_('rainfall'),_('snow'),_('snow'),_('thunderstorm'),_('no data'),_('w/o precipitation'));
+        $this->direction_arr = array('N','NE','E','SE','S','SW','W','NW');
     }
     
     public function getData(){
@@ -39,8 +46,10 @@ class Gismeteo
                 $tod_id = strval($item->attributes()->tod);
                 $tod = $this->tod_arr[$tod_id];
                 $day = strval($item->attributes()->day);
-                $months = $this->month_arr[intval($item->attributes()->month)];
-                $weekday = $this->weekday_arr[strval($item->attributes()->weekday)];
+                $month_id = intval($item->attributes()->month);
+                $months = $this->month_arr[$month_id];
+                $weekday_id = strval($item->attributes()->weekday);
+                $weekday = $this->weekday_arr[$weekday_id];
                 $title = $tod.' '.$day.' '.$months.', '.$weekday;
                 
                 $t_min = strval($item->TEMPERATURE->attributes()->min);
@@ -66,16 +75,35 @@ class Gismeteo
                 
                 $pressure = strval($item->PRESSURE->attributes()->min).'..'.strval($item->PRESSURE->attributes()->max);
                 $pressure_str = $pressure.' мм рт.ст.';
-                
-                $wind = $this->direction_arr[strval($item->WIND->attributes()->direction)].', '.strval($item->WIND->attributes()->min).'-'.strval($item->WIND->attributes()->max);
+
+                $wind_from_id = strval($item->WIND->attributes()->direction);
+                $wind_from = $this->direction_arr[$wind_from_id];
+                $wind_min = strval($item->WIND->attributes()->min);
+                $wind_max = strval($item->WIND->attributes()->max);
+                $wind = $wind_from.', '.$wind_min.'-'.$wind_max;
                 $wind_str = $wind.' м/с';
                 
-                $gis_arr[$i]['title'] = $title;
+                $gis_arr[$i]['title']       = $title;
+                $gis_arr[$i]['tod']         = $tod;
+                $gis_arr[$i]['tod_id']      = $tod_id;
+                $gis_arr[$i]['day']         = $day;
+                $gis_arr[$i]['months']      = $months;
+                $gis_arr[$i]['month_id']    = $month_id;
+                $gis_arr[$i]['weekday']     = $weekday;
+                $gis_arr[$i]['weekday_id']  = $weekday_id;
                 $gis_arr[$i]['temperature'] = $temperature;
                 $gis_arr[$i]['phenomena']   = $phenomena;
                 $gis_arr[$i]['pressure']    = $pressure;
+                $gis_arr[$i]['wind_from_id']= $wind_from_id;
+                $gis_arr[$i]['wind_from']   = $wind_from;
+                $gis_arr[$i]['wind_min']    = $wind_min;
+                $gis_arr[$i]['wind_max']    = $wind_max;
                 $gis_arr[$i]['wind']        = $wind;
-                $gis_arr[$i]['description'] = $phenomena.',<br>давление '.$pressure_str.', ветер '.$wind_str;
+                $gis_arr[$i]['cloudiness']    = $cloudiness;
+                $gis_arr[$i]['cloudiness_id']    = $cloudiness_id;
+                $gis_arr[$i]['precipitation']    = $precipitation;
+                $gis_arr[$i]['precipitation_id'] = $precipitation_id;
+                $gis_arr[$i]['description'] = $phenomena.',<br>'._('pressure').' '.$pressure_str.', '._('wind').' '.$wind_str;
                 if ($tod_id == 0 || $tod_id == 3){
                     $img_1 = 'w_moon.png';
                 }else{
@@ -102,6 +130,19 @@ class Gismeteo
         $content = unserialize(System::base64_decode($content));
         
         if (is_array($content)){
+
+            $content = array_map(function($day){
+
+                $day['title'] = _($day['tod']).' '.$day['day'].' '._($day['months']).', '._($day['weekday']);
+                $day['phenomena']   = _($day['cloudiness']).', '._($day['precipitation']);
+                //var_dump($day['wind_from']);
+                $day['wind'] = _($day['wind_from']).', '.$day['wind_min'].'-'.$day['wind_max'];
+
+                return $day;
+
+            },
+            $content);
+
             return $content;
         }else{
             return 0;
