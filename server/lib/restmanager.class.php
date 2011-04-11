c8ffe88c3c1607af63a49bb40a0006139fc384b5
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-11 11:08:25 +0000

    Added RESTManager class

---
+++ server/lib/restmanager.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class RESTManager
+{
+
+    private function __construct(){}
+
+    public static function handleRequest(){
+
+        ob_start();
+
+        $result = null;
+
+        $response = new RESTResponse();
+
+        try{
+            $request  = new RESTRequest();
+
+            $cmd_r  = new RESTCommandResolver();
+            $cmd    = $cmd_r->getCommand($request);
+            $result = $cmd->execute($request);
+            
+        }catch (Exception $e){
+            $response->setError($e->getMessage());
+        }
+
+        $response->setBody($result);
+        $response->send();
+    }
+}
+
+?>
