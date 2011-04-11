2957e3b584c74aca9e6833a334ee0c2507fa4a9b
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-11 11:09:33 +0000

    Added RESTCommandItv class

---
+++ server/lib/restcommanditv.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class RESTCommandItv extends RESTCommand
+{
+    private $manager;
+    private $allowed_fields;
+
+    public function __construct(){
+        $this->manager = Itv::getInstance();
+        $this->allowed_fields = array_fill_keys(array('id', 'name', 'number', 'base_ch'), true);
+    }
+
+    public function get(RESTRequest $request){
+
+        $itv_list = $this->manager->getByIds($request->getIdentifiers());
+
+        $allowed_fields = $this->allowed_fields;
+
+        $itv_list = array_map(function($item) use ($allowed_fields){
+
+            $item = array_intersect_key($item, $allowed_fields);
+
+            return $item;
+        },
+        $itv_list
+        );
+
+        return $itv_list;
+    }
+}
+
+?>
