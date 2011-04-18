76a23b81b956cf05710479319a930ae3547e7e1c
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-18 08:51:30 +0000

    Removed using the old configuration file

--- server/api/testers_macs.php
+++ server/api/testers_macs.php
@@ -1,6 +1,5 @@
 <?php
 
-include "../conf_serv.php";
 include "../common.php";
 
 echo 'api_callback('.json_encode(Mysql::getInstance()->from('testers')->where(array('status' => 1))->get()->all('mac')).')';
