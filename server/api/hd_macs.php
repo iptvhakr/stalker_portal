10490f91319da579f0e6ccbbf15f206043bb0431
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-18 08:51:08 +0000

    Removed using the old configuration file

--- server/api/hd_macs.php
+++ server/api/hd_macs.php
@@ -1,6 +1,5 @@
 <?php
 
-include "../conf_serv.php";
 include "../common.php";
 
 echo 'api_callback('.json_encode(Mysql::getInstance()->from('users')->where(array('hd' => 1))->get()->all('mac')).')';
