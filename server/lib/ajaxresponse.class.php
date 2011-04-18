20713b79fa1ca7944b572d8d616e063b074066d4
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-18 08:56:21 +0000

    Added new constant max_page_items

--- server/lib/ajaxresponse.class.php
+++ server/lib/ajaxresponse.class.php
@@ -6,7 +6,7 @@
  * @author zhurbitsky@gmail.com
  */
 
-class AjaxResponse
+abstract class AjaxResponse
 {
     
     protected $db;
@@ -17,7 +17,7 @@ class AjaxResponse
     protected $selected_item = 0;
     protected $response = array(
                     'total_items'    => 0,
-                    'max_page_items' => MAX_PAGE_ITEMS,
+                    'max_page_items' => 0,
                     'selected_item'  => 0,
                     'cur_page'       => 0,
                     'data'           => array(),
@@ -27,24 +27,22 @@ class AjaxResponse
     protected $months = array();
     protected $all_title = '';
     protected $no_ch_info = '';
+    const max_page_items = 14;
     
-    protected function __construct(){
+    public function __construct(){
         
         $this->db  = Mysql::getInstance();
         $this->stb = Stb::getInstance();
-        
-        //$this->abc = System::word('all_abc');
+
+        $this->response['max_page_items'] = self::max_page_items;
 
         /// TRANSLATORS: Letters of the alphabet. If the letter is missing - leave ".";
         $this->abc = array_filter(array('*',_('ABC_1l'),_('ABC_2l'),_('ABC_3l'),_('ABC_4l'),_('ABC_5l'),_('ABC_6l'),_('ABC_7l'),_('ABC_8l'),_('ABC_9l'),_('ABC_10l'),_('ABC_11l'),_('ABC_12l'),_('ABC_13l'),_('ABC_14l'),_('ABC_15l'),_('ABC_16l'),_('ABC_17l'),_('ABC_18l'),_('ABC_19l'),_('ABC_20l'),_('ABC_21l'),_('ABC_22l'),_('ABC_23l'),_('ABC_24l'),_('ABC_25l'),_('ABC_26l'),_('ABC_27l'),_('ABC_28l'),_('ABC_29l'),_('ABC_30l'),_('ABC_31l'),_('ABC_32l'),_('ABC_33l')), function($e){return $e != '.';});
 
-        //$this->months =  System::word('all_months');
         $this->months =  array(_('january'),_('february'),_('march'),_('april'),_('may'),_('june'),_('july'),_('august'),_('september'),_('october'),_('november'),_('december'));
 
-        //$this->all_title =  System::word('all_title');
         $this->all_title = _('All');
 
-        //$this->no_ch_info = System::word('no_ch_info');
         $this->no_ch_info = _('[No channel info]');
 
         $this->page = @intval($_REQUEST['p']);
@@ -106,8 +104,8 @@ class AjaxResponse
      */
     protected function getImgUri($id){
     
-        $dir_name = ceil($id/FILES_IN_DIR);
-        $dir_path = IMG_URI.$dir_name;
+        $dir_name = ceil($id/100);
+        $dir_path = Config::get('portal_url').'screenshots/'.$dir_name;
         $dir_path .= '/'.$id.'.jpg';
         return $dir_path;
     }
