5eb7028a8aaff45ef3f103212f89b4cbc16eb3af
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-18 09:22:01 +0000

    Set as deprecated

--- server/tasks/update_day_weather.php
+++ server/tasks/update_day_weather.php
@@ -1,10 +1,10 @@
 <?php
-/*
-    
-*/
+/**
+ * @deprecated since version 4.7.3. Use update_weatherco_fullcurrent.php and update_weatherco_fullforecast.php
+ */
+
 error_reporting(E_ALL);
 
-include "../conf_serv.php";
 include "../common.php";
 
 $weather = new Gismeteo();
