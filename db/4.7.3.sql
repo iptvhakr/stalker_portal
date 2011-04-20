36d77520677322ea755c8fb23908adb02c65bb78
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-04-20 08:12:03 +0000

    Added changelog table

--- db/4.7.3.sql
+++ db/4.7.3.sql
@@ -1,1 +1,13 @@
 alter table screenshots drop path;
+
+CREATE TABLE IF NOT EXISTS changelog (
+  change_number BIGINT NOT NULL,
+  delta_set VARCHAR(10) NOT NULL,
+  start_dt TIMESTAMP NOT NULL,
+  complete_dt TIMESTAMP NULL,
+  applied_by VARCHAR(100) NOT NULL,
+  description VARCHAR(500) NOT NULL,
+);
+
+INSERT INTO `changelog` VALUES (1,'Main','2011-04-19 10:41:11','2011-04-19 10:41:11','dbdeploy','1-initial_schema.sql'),
+(2,'Main','2011-04-19 17:41:11','2011-04-19 17:41:11','dbdeploy','2-cities.sql');
