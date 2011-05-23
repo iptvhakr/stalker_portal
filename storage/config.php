f283b856de08d577bf8531eec4608a4ee87f5014
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-05-23 13:04:09 +0000

    Added RECORDS_DIR

--- storage/config.php
+++ storage/config.php
@@ -1,8 +1,9 @@
 <?php
 
-define('MASTER_IP', '10.128.1.1');
+define('MASTER_IP', '192.168.1.71');
 define('VIDEO_STORAGE_DIR', '/media/raid0/storage/');
 define('KARAOKE_STORAGE_DIR', '/media/raid0/karaoke/');
+define('RECORDS_DIR', '/media/raid0/records/');
 define('NFS_HOME_PATH', '/media/raid0/mac/');
 
 ?>
