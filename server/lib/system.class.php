a26b4002157b9858c0fc7a8b34f63d0935a98cc8
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-05-23 13:02:07 +0000

    Added helper methods

--- server/lib/system.class.php
+++ server/lib/system.class.php
@@ -3,12 +3,10 @@
 class System
 {
     
-    
-    //private static $words = array();
-    
     /**
      * Encodes data whith MIME base64 safe for url.
      *
+     * @static
      * @param string $input
      * @return string
      */
@@ -21,6 +19,7 @@ class System
     /**
      * Decodes data encoded by System::base64_encode().
      *
+     * @static
      * @param string $input
      * @return string
      */
@@ -30,40 +29,81 @@ class System
     }
     
     /**
-     * Sets the value of the variable $words.
-     *
-     * @param array $words_arr
-     */
-    /*public static function set_words($words_arr){
-        
-        self::$words = $words_arr;
-    }*/
-    
-    /**
-     * Returns a string according to the localization.
-     *
-     * @param string $alias
-     * @return string | null
-     */
-    /*public static function word($alias){
-        
-        if (key_exists($alias, self::$words)){
-            return self::$words[$alias];
-        }
-        
-        return null;
-    }*/
-    
-    /**
      * Returns all words.
      *
+     * @static
      * @return array $words
      */
     public static function get_all_words(){
 
-        require_once "lang/stb.php";
+        require_once PROJECT_PATH."/lang/stb.php";
 
         return $words;
+    }
+
+    /**
+     * Convert seconds to human readable length.
+     *
+     * echo System::convertTimeLengthToHuman(100);
+     * > 1:40
+     *
+     * echo System::convertTimeLengthToHuman(500);
+     * > 8:20
+     *
+     * @static
+     * @param int $length seconds
+     * @return string
+     */
+    public static function convertTimeLengthToHuman($length){
+
+        $hh = floor($length / 3600);
+
+        $mm = floor(($length - $hh*3600)/60);
+
+        $ss = $length - $hh*3600 - $mm*60;
+
+        $result = '';
+
+        if ($hh > 0){
+            /// Hours
+            $result .= $hh._('h').' ';
+        }
+
+        if ($mm > 0){
+            /// Minutes
+            $result .= $mm._('m').' ';
+        }
+
+        if ($ss > 0){
+            /// Seconds
+            $result .= $ss._('s').' ';
+        }
+
+        return $result;
+    }
+
+    /**
+     * Convert datetime to human readable date.
+     *
+     * @static
+     * @param int|string $datetime
+     * @return string
+     */
+    public static function convertDatetimeToHuman($datetime){
+
+        $this_mm = date("m");
+        $this_dd = date("d");
+        $this_yy = date("Y");
+
+        if ($datetime > mktime(0,0,0, $this_mm, $this_dd, $this_yy)){
+            $human_date = _('today').', '.date("H:i", $datetime);
+        }elseif ($datetime > mktime(0,0,0, $this_mm, $this_dd-1, $this_yy)){
+            $human_date = _('yesterday').', '.date("H:i", $datetime);
+        }else{
+            $human_date = date("d.m.Y H:i", $datetime);
+        }
+
+        return $human_date;
     }
 }
 
