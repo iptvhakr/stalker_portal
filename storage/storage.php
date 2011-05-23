3315c6272124c74d688c16402974ff5352dbfcd2
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-05-23 13:05:43 +0000

    Modified soap handler class

--- storage/storage.php
+++ storage/storage.php
@@ -4,10 +4,11 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 require_once("./config.php");
-require_once("./lib/storage.class.php");
+require_once("./lib/storage_soap.class.php");
 
-$server = new SoapServer('http://'.MASTER_IP.'/stalker_portal/server/storage/storage.wsdl.php');
-$server->setClass("Storage");
+//$server = new SoapServer('http://'.MASTER_IP.'/stalker_portal/server/storage/storage.wsdl.php');
+$server = new SoapServer(null, array('uri' => 'urn:storage', 'soap_version' => SOAP_1_1));
+$server->setClass("Storage_soap");
 $server->handle();
 
 ?>
