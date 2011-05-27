fd93d901452a49d23d002b313cbd6fe0f77abdfa
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-05-27 14:23:23 +0000

    Remove extra comma

--- db/4.7.3.sql
+++ db/4.7.3.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `changelog` (
   `start_dt` TIMESTAMP NOT NULL,
   `complete_dt` TIMESTAMP NULL,
   `applied_by` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(500) NOT NULL,
+  `description` VARCHAR(500) NOT NULL
 );
 
 INSERT INTO `changelog` VALUES (1,'Main','2011-04-19 10:41:11','2011-04-19 10:41:11','dbdeploy','1-initial_schema.sql'),
