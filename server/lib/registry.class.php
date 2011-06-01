d6d75e5cee3f7167efef0b14587eb966998f9023
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-06-01 15:20:51 +0000

    Added registry.class.php

---
+++ server/lib/registry.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class Registry{
+
+    protected static $data = array();
+
+    public static function get($key, $default = null){
+        return isset(self::$data[$key]) ? self::$data[$key] : $default;
+    }
+
+    public static function set($key, $value){
+        self::$data[$key] = $value
+    }
+}
+
+?>
