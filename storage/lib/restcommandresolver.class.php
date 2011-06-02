19ff127e307487c96af31d1ff76dc64e19dd375f
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-06-02 10:45:14 +0000

    Modified getCommand for php < 5.3.0

--- storage/lib/restcommandresolver.class.php
+++ storage/lib/restcommandresolver.class.php
@@ -12,9 +12,16 @@ class RESTCommandResolver
      */
     public function getCommand(RESTRequest $request){
 
-        $resource = implode(array_map(function($part){
+        //For php >= 5.3.0
+        /*$resource = implode(array_map(function($part){
             return ucfirst($part);
-        },explode('_', $request->getResource())));
+        },explode('_', $request->getResource())));*/
+
+        $resource = '';
+
+        foreach (explode('_', $request->getResource()) as $part){
+            $resource .= ucfirst($part);
+        }
 
         $class = 'RESTCommand'.ucfirst($resource);
 
