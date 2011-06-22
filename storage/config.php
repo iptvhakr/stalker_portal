4ec865cf3c63b7c33aa6133e60bf8b1db7fdef49
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-06-22 10:23:06 +0000

    Added new settings

--- storage/config.php
+++ storage/config.php
@@ -1,9 +1,10 @@
 <?php
 
-define('MASTER_IP', '192.168.1.71');
 define('VIDEO_STORAGE_DIR', '/media/raid0/storage/');
 define('KARAOKE_STORAGE_DIR', '/media/raid0/karaoke/');
 define('RECORDS_DIR', '/media/raid0/records/');
 define('NFS_HOME_PATH', '/media/raid0/mac/');
+define('TASKS_API_URL', 'http://bb3.sandbox/current/api/tv_archive/');
+define('STORAGE_NAME', 'bb3');
 
 ?>
