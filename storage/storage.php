87dbbebb5f0ccb3b4c259b36c93717121acd488b
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-06-22 10:24:40 +0000

    Removed comments

--- storage/storage.php
+++ storage/storage.php
@@ -6,7 +6,6 @@ error_reporting(E_ALL);
 require_once("./config.php");
 require_once("./lib/storage_soap.class.php");
 
-//$server = new SoapServer('http://'.MASTER_IP.'/stalker_portal/server/storage/storage.wsdl.php');
 $server = new SoapServer(null, array('uri' => 'urn:storage', 'soap_version' => SOAP_1_1));
 $server->setClass("Storage_soap");
 $server->handle();
