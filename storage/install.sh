923d1848f0db67c2c7aab20d902f69156b282059
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-06-22 10:21:16 +0000

    Added install.sh

---
+++ storage/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+BASEDIR="$0"
+BASEDIR="${BASEDIR%/*}"
+BASEDIR=`cd $BASEDIR; pwd`
+
+sed "s%@STORAGE_PATH@%$BASEDIR%" ./src/tvarchivetasks.conf > /etc/init/tvarchivetasks.conf
+start tvarchivetasks
