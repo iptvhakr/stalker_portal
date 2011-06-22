0ef3245d08205315ffc652a83e4296e8d0f70366
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-06-22 10:21:08 +0000

    Added restcommandtvarchiverecorder.class.php

---
+++ storage/lib/restcommandtvarchiverecorder.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class RESTCommandTvArchiveRecorder extends RESTCommand
+{
+    private $manager;
+
+    public function __construct(){
+        $this->manager = new TvArchiveRecorder();
+    }
+
+    /**
+     * Create recording task
+     *
+     * @param RESTRequest $request
+     * @return bool
+     */
+    public function create(RESTRequest $request){
+
+        $task = $request->getData('task');
+
+        if (empty($task)){
+            throw new ErrorException('Empty task');
+        }
+
+        return $this->manager->start($task);
+    }
+
+    /**
+     * Delete recording task
+     *
+     * @param RESTRequest $request
+     * @return bool
+     */
+    public function delete(RESTRequest $request){
+
+        $ch_ids = $request->getIdentifiers();
+
+        if (empty($ch_ids[0])){
+            throw new ErrorException('Empty ch_id');
+        }
+
+        return $this->manager->stop($ch_ids[0]);
+    }
+}
