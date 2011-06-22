1cb493d47f0e0e2efba3815b6fad1b6d364af2fd
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-06-22 10:13:50 +0000

    Added delta sql script

---
+++ db/delta/4-tv_archive.sql
@@ -0,0 +1,14 @@
+--
+ALTER TABLE `itv` ADD `enable_tv_archive` tinyint default 0;
+
+CREATE TABLE IF NOT EXISTS `tv_archive`(
+    `id` int NOT NULL auto_increment,
+    `ch_id` int NOT NULL default 0,
+    `storage_name` varchar(128) NOT NULL default '',
+    `start_time` timestamp default 0,
+    `end_time` timestamp default 0,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`ch_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--//@UNDO
