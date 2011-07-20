9670318a97a99cf5e81d6f9c2d986f73f2a13029
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-07-20 13:30:45 +0000

    Modified image path

--- c/infoportal.js
+++ c/infoportal.js
@@ -6,7 +6,7 @@
     
     var submenu = module.infoportal_sub || [];
     
-    main_menu.add(word['infoportal_title'], submenu, 'i/mm_ico_info.png', '', {"layer_name" : "infoportal"});
+    main_menu.add(word['infoportal_title'], submenu, 'mm_ico_info.png', '', {"layer_name" : "infoportal"});
     
 })();
 
