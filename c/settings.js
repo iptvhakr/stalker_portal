2ef37032f54ce8f0173c2c2b60ff8f2308a03cdb
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-07-20 13:41:29 +0000

    Modified images path

--- c/settings.js
+++ c/settings.js
@@ -6,7 +6,7 @@
     
     var submenu = module.settings_sub || [];
     
-    main_menu.add(word['settings_title'], submenu, 'i/mm_ico_setting.png', '', {"layer_name" : "settings"});
+    main_menu.add(word['settings_title'], submenu, 'mm_ico_setting.png', '', {"layer_name" : "settings"});
     
 })();
 
