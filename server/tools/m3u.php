08108c9306d96f4062a3034be878b06a052546fc
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-10-07 11:36:19 +0000

    Added m3u raw output

--- server/tools/m3u.php
+++ server/tools/m3u.php
@@ -1,6 +1,8 @@
 <?php
 set_time_limit(0);
 
+ob_start();
+
 include "../common.php";
 
 $channels = Mysql::getInstance()->from('itv')->where(array('status' => 1))->orderby('number')->get()->all();
@@ -9,9 +11,26 @@ $m3u_data = "#EXTM3U\n";
 
 foreach ($channels as $channel){
     $m3u_data .= "#EXTINF:0,".$channel['name']."\n";
-    $m3u_data .= $channel['cmd']."\n";
+
+    if (!empty($_GET['origin'])){
+        $m3u_data .= $channel['cmd']."\n";
+    }else{
+        if (preg_match("/([^\s]+:\/\/[^\s]+)/", $channel['cmd'], $tmp)){
+            $cmd = $tmp[1];
+        }else{
+            $cmd = '';
+        }
+        $m3u_data .= $cmd."\n";
+    }
 }
 
-file_put_contents(PROJECT_PATH.'/tv.m3u', $m3u_data);
+if (is_file(PROJECT_PATH.'/tv.m3u') && is_writable(PROJECT_PATH.'/tv.m3u') || is_writable(PROJECT_PATH)){
+    @file_put_contents(PROJECT_PATH.'/tv.m3u', $m3u_data);
+}
+
+header('Content-Type: audio/mpegurl');
+header('Content-Disposition: attachment; filename="playlist.m3u"');
+
+echo $m3u_data;
 
 ?>
