a6c437aa00e6af01ded0be9a602fe89e87a45b96
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-11-03 15:04:35 +0000

    Added accountinfo.class.php

---
+++ server/lib/accountinfo.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class AccountInfo
+{
+    private $stb;
+
+    public function __construct(){
+        $this->stb = Stb::getInstance();
+    }
+
+    public function getMainInfo(){
+        return sprintf(_('account_main_info'),
+            $this->stb->getParam('fname'),
+            $this->stb->getParam('last_change_status'),
+            $this->stb->getParam('phone'));
+    }
+
+    public function getPaymentInfo(){
+        return _('account_payment_info');
+    }
+
+    public function getAgreementInfo(){
+        return _('account_agreement_info');
+    }
+
+    public function getTermsInfo(){
+        return _('account_terms_info');
+    }
+}
+
+?>
