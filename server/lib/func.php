662418edaf1c6b19f910762434be90cb5bd0cb04
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-11-11 15:56:38 +0000

    Modified transliterate function

--- server/lib/func.php
+++ server/lib/func.php
@@ -1,14 +1,5 @@
 <?php
 
-/*function __autoload($class_name) {
-    $class = PORTAL_PATH.'/server/lib/'.strtolower($class_name).'.class.php';
-    if (file_exists($class)){
-        require_once $class;
-    }else{
-        trigger_error('Class "'.$class_name.'" not exist in '.$class);
-    }
-}*/
-
 function get_save_folder($id){
     
     $dir_name = ceil($id/100);
@@ -27,23 +18,8 @@ function get_save_folder($id){
     
 }
 
-/*function get_img_uri($id){
-    
-    $dir_name = ceil($id/100);
-    $dir_path = IMG_URI.$dir_name;
-    $dir_path .= '/'.$id.'.jpg';
-    return $dir_path;
-}*/
-
 function transliterate($st) {
-   //var_dump($st);
-   //$st = utf8_decode($st);
 
-   /*$st = strtr($st,
-        utf8_encode("абвгдежзийклмнопрстуфыэАБВГДЕЖЗИЙКЛМНОПРСТУФЫЭ"),
-        "abvgdegziyklmnoprstufieABVGDEGZIYKLMNOPRSTUFIE"
-   );*/
-   
    $st = trim($st);
    
    $st = strtr($st, array(
@@ -101,17 +77,10 @@ function transliterate($st) {
         '\''=>"", '\\'=>"", '/'=>"", ';'=>"", ':'=>"", '«'=>"", '»'=>"", '`'=>"", '-' => "-", '—' => "-"
    ));
 
+   $st = preg_replace("/[^a-z0-9_-]/i", "", $st);
+
    return $st;
 }
-
-/*function rename_incoming_dir($old_name, $new_name){
-    echo 'rename_incoming_dir '.$old_name. ' -> '.$new_name;
-    if ($old_name == ''){
-        return create_incoming_dir($new_name);
-    }else if($old_name != $new_name){
-        return rename(INCOMING_DIR.$old_name, INCOMING_DIR.$new_name);
-    }
-}*/
 
 function check_db_user_login($login, $pass){
     $db = new Database();
@@ -149,14 +118,6 @@ function check_session_user_login(){
     }
 }
 
-/*function check_moderator_login(){
-    if (@$_SESSION['adm_login'] != MODERATOR_LOGIN && @$_SESSION['adm_password'] != MODERATOR_PASSWORD){        
-        return 0;
-    }else{
-        return 1;
-    }
-}*/
-
 function moderator_access(){
     if(!check_session_user_login()){
         header("Location: login.php");
@@ -174,44 +135,6 @@ function check_access($num = array()){
 }
 
 function get_cur_playing_type($db, $in_param = ''){
-    /*$now_timestamp = time() - 120;
-    $now_time = date("Y-m-d H:i:s", $now_timestamp);
-    
-    $query = "select mac from users where keep_alive>'$now_time'";
-    $rs=$db->executeQuery($query);
-    
-    if($in_param == ''){
-        $in_param = $_GET['in_param'];
-    }
-    
-    $cur_play['itv'] = 0;
-    $cur_play['vclub'] = 0;
-    $cur_play['karaoke'] = 0;
-    $cur_play['aclub'] = 0;
-    $cur_play['radio'] = 0;
-    
-    while(@$rs->next()){
-        $arr=$rs->getCurrentValuesAsHash();
-        $sql = "select type from user_log where mac='{$arr['mac']}' order by time desc limit 0,1";
-        $rs2=$db->executeQuery($sql);
-        $type = $rs2->getValueByName(0, 'type');
-        if ($type == 1){
-            $cur_play['itv']++;
-        }else if($type == 2){
-            $cur_play['vclub']++;
-        }else if($type == 3){
-            $cur_play['karaoke']++;
-        }else if($type == 4){
-            $cur_play['aclub']++;
-        }else if($type == 5){
-            $cur_play['radio']++;
-        }
-    }
-    if (in_array($in_param, $cur_play)){
-        return $cur_play[$in_param];
-    }else{
-        return "wrong input param";
-    }*/
     return get_cur_active_playing_type($db, $in_param);
 }
 
@@ -219,22 +142,10 @@ function get_cur_active_playing_type($db, $in_param = ''){
     $now_timestamp = time() - 120;
     $now_time = date("Y-m-d H:i:s", $now_timestamp);
     
-    /*$query = "select mac from users where keep_alive>'$now_time'";
-    $rs=$db->executeQuery($query);*/
-    
     if($in_param == ''){
         $in_param = $_GET['in_param'];
     }
-    
-/*    $cur_play['itv'] = 0;
-    $cur_play['vclub'] = 0;
-    $cur_play['karaoke'] = 0;
-    $cur_play['aclub'] = 0;
-    $cur_play['radio'] = 0;*/
-    
-    /*$from_timestamp = time() - 7200;
-    $from_time = date("Y-m-d H:i:s", $from_timestamp);*/
-    
+
     if ($in_param == 'itv'){
         $type = 1;
     }else if($in_param == 'vclub'){
@@ -252,28 +163,7 @@ function get_cur_active_playing_type($db, $in_param = ''){
     $sql = "select count(*) as counter from users where UNIX_TIMESTAMP(keep_alive) > $now_timestamp and now_playing_type=$type";
     $rs=$db->executeQuery($sql);
     $counter = $rs->getValueByName(0, 'counter');
-    /*while(@$rs->next()){
-        $arr=$rs->getCurrentValuesAsHash();
-        $sql = "select type from user_log where mac='{$arr['mac']}' and time>'$from_time' order by time desc limit 0,1";
-        $rs2=$db->executeQuery($sql);
-        $type = $rs2->getValueByName(0, 'type');
-        if ($type == 1){
-            $cur_play['itv']++;
-        }else if($type == 2){
-            $cur_play['vclub']++;
-        }else if($type == 3){
-            $cur_play['karaoke']++;
-        }else if($type == 4){
-            $cur_play['aclub']++;
-        }else if($type == 5){
-            $cur_play['radio']++;
-        }
-    }
-    if (in_array($in_param, $cur_play)){
-        return $cur_play[$in_param];
-    }else{
-        return "wrong input param";
-    }*/
+
     return $counter;
 }
 
@@ -338,12 +228,6 @@ function datetime2timestamp($datetime){
     return @mktime($arr[4], $arr[5], $arr[6], $arr[2], $arr[3], $arr[1]);
 }
 
-/*function xmltvdatetime2datetime($datetime){
-    $date  = substr($datetime, 0,4)."-".substr($datetime, 4,2)."-".substr($datetime, 6,2);
-    $date .= " ".substr($datetime,8,2).":".substr($datetime,10,2).":".substr($datetime,12,2);
-    return $date;
-}*/
-
 function get_str_lang($str){
     $lang = 0;
     $first_l = substr($str, 0, 1);
@@ -356,19 +240,6 @@ function get_str_lang($str){
     }
     return $lang;
 }
-
-/*function get_first_mpg($path){
-    if ($handle = @opendir($path)) {
-        while (false !== ($file = readdir($handle))) {
-            if ($file != "." && $file != ".." && preg_match("/([\S\s]+).mpg$/", $file)) { 
-                closedir($handle); 
-                return $file;
-            } 
-        }
-    }
-    @closedir($handle); 
-    return 0;
-}*/
 
 function js_redirect($to, $msg = '', $delay = 2){
    ?>   
@@ -437,25 +308,6 @@ function get_bonus_channels($id = 0){
     }
 }
 
-/*function get_base_ch(){
-    $db = Database::getInstance();
-    
-    $sql = "select * from itv where base_ch=1";
-    $rs = $db->executeQuery($sql);
-    $bonus_ch = $rs->getValuesByName('id');
-    return $bonus_ch;
-}
-
-function get_all_subscription_and_base_ch($uid){
-    //echo "get_all_subscription_and_base_ch";
-    $sub_ch   = get_sub_channels($uid);
-    $bonus_ch = get_bonus_channels($uid);
-    $base_ch = get_base_ch();
-    
-    $total_subscription = array_merge($sub_ch, $bonus_ch, $base_ch);
-    return $total_subscription;
-}*/
-
 function kop2grn($kops){
     $grn = floor($kops/100);
     $kop = $kops - $grn*100;
@@ -464,53 +316,6 @@ function kop2grn($kops){
     }
     return $grn.'.'.$kop;
 }
-
-/*function get_operator_id_by_stb_ip($ip){
-    $db = Database::getInstance();
-    
-    $long_ip = long2ip($ip);
-    $sql = "select * from operators_ip 
-                  where
-                   long_ip_from<$ip
-               and long_ip_to>$ip";
-    
-    $rs   = $db->executeQuery($sql);
-    $rows = $rs->getRowCount();
-    
-    if ($rows == 1){
-        $operator_id = $rs->getValuesByName('operator_id');
-    }else{
-        $operator_id = 1;
-    }
-    return $operator_id;
-}*/
-
-/*function get_ip_range($ip_n_mask){
-    $mask = 0xFFFFFFFF;
-    
-    $ip = explode("/", $ip_n_mask);
-    
-    for ($j = 0; $j < 32 - $ip[1]; $j++){
-        $mask = $mask << 1;
-    }
-    
-    $long_ip = ip2long($ip[0]);
-    
-    $ip_range = array();
-    $ip_range['from'] = long2ip($long_ip&$mask);
-    $ip_range['to']   = long2ip(($long_ip&$mask)+(~$mask));
-    return $ip_range;
-}*/
-
-/*
-function save_series($arr, $path){
-    $db = Database::getInstance();
-    
-    sort($arr);
-    $query = "update video set series='".serialize($arr)."' where path='$path'";
-    $rs=$db->executeQuery($query);
-}
-*/
 
 function set_video_status($id, $val){
     $db = Database::getInstance();
@@ -539,34 +344,4 @@ function get_storage_use($db, $in_param = ''){
     return $status;
 }
 
-/*function get_anec_rating($id){
-    $db = Database::getInstance();
-    $sql = "select count(*) as count from anec_rating where anec_id=$id";
-    $rs1 = $db->executeQuery($sql);
-    $rating = @intval($rs1->getValueByName(0, 'count'));
-    return $rating;
-}*/
-
-/*function get_anec_voted($id, $stb_id){
-    $db = Database::getInstance();
-    $sql = "select count(*) as count from anec_rating where uid=$stb_id and anec_id=$id";
-    $rs1 = $db->executeQuery($sql);
-    if (@intval($rs1->getValueByName(0, 'count')) == 1){
-        return 1;
-    }else{
-        return 0;
-    }
-}*/
-
-/*function normalize_mac($mac){
-    $pattern = array('А', 'В', 'С', 'Е'); // ru
-    $replace = array('A', 'B', 'C', 'E'); // en
-    
-    $mac = str_replace($pattern, $replace, trim($mac));
-    
-    if (strlen($mac)==12){
-        $mac = substr($mac, 0,2).":".substr($mac, 2,2).":".substr($mac, 4,2).":".substr($mac, 6,2).":".substr($mac, 8,2).":".substr($mac, 10,2);
-    }
-    return $mac;
-}*/
 ?>
