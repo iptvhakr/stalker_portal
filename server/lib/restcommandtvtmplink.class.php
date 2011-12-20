64121d29d3447b2d3427f38997427937c45c1d8a
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2011-12-20 13:03:40 +0000

    Added restcommandtvtmplink.class.php

---
+++ server/lib/restcommandtvtmplink.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class RESTCommandTvTmpLink extends RESTCommand
+{
+
+    public function get(RESTRequest $request){
+
+        $ids = $request->getIdentifiers();
+
+        if (empty($ids[0])){
+            throw new ErrorException('Empty token');
+        }
+
+        return Itv::checkTemporaryLink($ids[0]);
+    }
+}
+
+?>
