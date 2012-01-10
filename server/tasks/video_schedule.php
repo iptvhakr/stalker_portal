971bb9d6d4bc2a83a97499ff9ac3026e38727c9c
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-01-10 13:42:28 +0000

    Added video scheduler

---
+++ server/tasks/video_schedule.php
@@ -0,0 +1,16 @@
+<?php
+
+include "../common.php";
+
+$today_tasks = Mysql::getInstance()->from('video_on_tasks')->where(array('date_on' => 'CURDATE()'))->get()->all();
+
+foreach ($today_tasks as $task){
+    try{
+        Video::switchOnById($task['video_id']);
+        Mysql::getInstance()->delete('video_on_tasks', array('id' => $task['id']));
+    }catch(Exception $e){
+        echo $e->getTraceAsString();
+    }
+}
+
+?>
