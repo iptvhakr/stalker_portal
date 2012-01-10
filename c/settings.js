60135ce3b7de97510ca783496a2ac3aeab506fa9
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-01-10 13:51:43 +0000

    Not modified

--- c/settings.js
+++ c/settings.js
@@ -7,6 +7,10 @@
     var submenu = module.settings_sub || [];
     
     main_menu.add(word['settings_title'], submenu, 'mm_ico_setting.png', '', {"layer_name" : "settings"});
+
+    /*main_menu.add(word['settings_title'], [], 'mm_ico_setting.png', function(){
+        
+    }, {"layer_name" : "settings"});*/
     
 })();
 
