e061c754b2512115fd39fd2b34626cc43819ef56
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-01-27 14:39:59 +0000

    Added external settings menu support

--- c/settings.js
+++ c/settings.js
@@ -3,15 +3,18 @@
  */
 
 (function(){
-    
-    var submenu = module.settings_sub || [];
-    
-    main_menu.add(word['settings_title'], submenu, 'mm_ico_setting.png', '', {"layer_name" : "settings"});
 
-    /*main_menu.add(word['settings_title'], [], 'mm_ico_setting.png', function(){
-        
-    }, {"layer_name" : "settings"});*/
-    
+    var submenu = module.settings_sub || [];
+
+    if (stb.firmware_version < 212){
+        main_menu.add(word['settings_title'], submenu, 'mm_ico_setting.png', '', {"layer_name" : "settings"});
+    }else{
+        main_menu.add(word['settings_title'], [], 'mm_ico_setting.png', function(){
+            _debug("http://" + stb.portal_ip +  "/" + stb.portal_path + "/external/settings/index.html");
+            stbWindowMgr.openWebFavorites("http://" + stb.portal_ip +  "/" + stb.portal_path + "/external/settings/index.html?ajax_loader="+stb.ajax_loader, 0);
+        }, {"layer_name" : "settings"});
+    }
+
 })();
 
 loader.next();
