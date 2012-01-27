f124c612b7dd548a8e7dbf11133600f4ef55d070
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-01-27 14:30:46 +0000

    Added authentication for API

--- api/index.php
+++ api/index.php
@@ -2,6 +2,17 @@
 
 require_once "../server/common.php";
 
+if (!Config::getSafe('enable_api', false) &&
+      (empty($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['REMOTE_ADDR'] : $_SERVER['HTTP_X_REAL_IP']) != $_SERVER['SERVER_ADDR'] &&
+      strpos($_SERVER['QUERY_STRING'], 'tv_archive') != 2){
+
+    header($_SERVER['SERVER_PROTOCOL']." 404 Not Found");
+    echo "API not enabled";
+    exit;
+}
+
+RESTManager::setAuthParams(Config::getSafe('api_auth_login', ''), Config::getSafe('api_auth_password', ''));
+RESTManager::enableLogger(Config::getSafe('enable_api_log', false));
 RESTManager::handleRequest();
 
 ?>
