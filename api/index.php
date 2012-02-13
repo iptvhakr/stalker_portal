006fb539fa12297b14ae20fd4d66d4fb693e70c4
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-02-13 09:08:35 +0000

    Added tv_tmp_link support

--- api/index.php
+++ api/index.php
@@ -4,7 +4,8 @@ require_once "../server/common.php";
 
 if (!Config::getSafe('enable_api', false) &&
       (empty($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['REMOTE_ADDR'] : $_SERVER['HTTP_X_REAL_IP']) != $_SERVER['SERVER_ADDR'] &&
-      strpos($_SERVER['QUERY_STRING'], 'tv_archive') != 2){
+      strpos($_SERVER['QUERY_STRING'], 'tv_archive') != 2 &&
+      strpos($_SERVER['QUERY_STRING'], 'tv_tmp_link') != 2){
 
     header($_SERVER['SERVER_PROTOCOL']." 404 Not Found");
     echo "API not enabled";
