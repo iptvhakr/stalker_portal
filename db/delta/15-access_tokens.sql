084661730adbee75ef8e677f780359f78450cf97
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-02-17 16:11:45 +0000

    New sql update

---
+++ db/delta/15-access_tokens.sql
@@ -0,0 +1,35 @@
+--
+CREATE TABLE IF NOT EXISTS `access_tokens`(
+    `id` int NOT NULL auto_increment,
+    `uid` int NOT NULL default 0,
+    `token` varchar(128) NOT NULL default '',
+    `expires` timestamp default 0,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`token`),
+    UNIQUE KEY (`uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `clients`(
+    `id` int NOT NULL auto_increment,
+    `secret` varchar(256) NOT NULL default '',
+    `description` text,
+    `added` datetime,
+    `active` tinyint default 1,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `image_update_settings`(
+    `id` int NOT NULL auto_increment,
+    `enable` tinyint default 0,
+    `require_image_version` varchar(32) NOT NULL default '',
+    `require_image_date` varchar(128) NOT NULL default '',
+    `image_version_contains` varchar(32) NOT NULL default '',
+    `image_description_contains` varchar(128) NOT NULL default '',
+    `update_type` varchar(64) NOT NULL default 'http_update',
+    `changed` timestamp not null,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE users modify image_version varchar(64) NOT NULL default '';
+
+--//@UNDO
