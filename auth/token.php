4cf38ed1e89ea9e4c3d863f1a1ba23b7ed5820e4
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-03-06 13:05:35 +0000

    Added mac and bearer token types support

--- auth/token.php
+++ auth/token.php
@@ -6,6 +6,5 @@ use Stalker\Lib\OAuth\AuthAccessHandler;
 use Stalker\Lib\OAuth\OAuthServer;
 
 $oauth_server = new OAuthServer(new AuthAccessHandler());
+$oauth_server->setTokenType(Config::getSafe("api_v2_access_type", "bearer"));
 $oauth_server->handleAuthRequest();
-
-?>
