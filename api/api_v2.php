e0fb21c15edb5f5c51503d4aeba41c43fc3ed629
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-03-06 13:04:40 +0000

    Added config

--- api/api_v2.php
+++ api/api_v2.php
@@ -2,6 +2,11 @@
 
 require_once "../server/common.php";
 
+if (!Config::getSafe('enable_api_v2', false)){
+    echo "API v2 not enabled";
+    exit;
+}
+
 use Stalker\Lib\RESTAPI\v2\RESTApiManager;
 use Stalker\Lib\OAuth\AuthAccessHandler;
 
