d45862282161ef1bc922070b8cef7f6b3c476113
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2012-08-17 07:12:40 +0000

    Added vidimax support (issue #1078)

---
+++ c/vidimax.js
@@ -0,0 +1,28 @@
+/**
+ * Redirection to vidimax module.
+ */
+(function(){
+
+    main_menu.add('VIDIMAX', [], 'mm_ico_vidimax.png', function(){
+
+        var params = '';
+
+        if (stb.user['web_proxy_host']){
+            params = '?proxy=http://'+stb.user['web_proxy_host']+':' +stb.user['web_proxy_port'];
+        }
+
+        stb.setFrontPanel('.');
+
+        if (!params){
+            params += '?';
+        }else{
+            params += '&';
+        }
+
+        params += 'referrer='+encodeURIComponent(window.location);
+
+        window.location = 'http://app-serv.vidimax.ru/mag/index.html'+params;
+    }, {layer_name : "vidimax"});
+
+    loader.next();
+})();
