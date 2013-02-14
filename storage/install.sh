7ed51ccc90a20cb361d1878b798974ad44865956
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2013-02-14 13:08:49 +0000

    Improve install script on storage (issue #1434)

--- storage/install.sh
+++ storage/install.sh
@@ -5,5 +5,8 @@ BASEDIR="$0"
 BASEDIR="${BASEDIR%/*}"
 BASEDIR=`cd $BASEDIR; pwd`
 
+touch .tasks
+chmod 666 .tasks
+
 sed "s%@STORAGE_PATH@%$BASEDIR%" ./src/tvarchivetasks.conf > /etc/init/tvarchivetasks.conf
 start tvarchivetasks
