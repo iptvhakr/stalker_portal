52588e732c7efcee41d1eaa7f11e5c804014bd86
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2013-03-15 11:23:22 +0000

    Fix pppoe settings (issue #1659)

--- c/keydown.keycodes.js
+++ c/keydown.keycodes.js
@@ -1,6 +1,6 @@
 var key = {
     
-    POWER : 2085,
+    POWER : 2085, // Alt+U
     MUTE  : 2192,
     MIC   : 2032,
     CLOCK : 2032,
@@ -15,44 +15,44 @@ var key = {
     NUM9  : 57,
     NUM0  : 48,
     MENU  : 122,
-    BACK  : 8,
-    CHANNEL_PREV  : 1009,
-    CHANNEL_NEXT  : 9,
-    EXIT  : 27,
-    REFRESH  : 116,
-    UP    : 38,
-    DOWN  : 40,
-    LEFT  : 37,
-    RIGHT : 39,
-    OK    : 13,
-    PAGE_NEXT : 34,
-    PAGE_PREV : 33,
-    NEXT  : 34,
-    PREV  : 33,
-    VOL_UP    : 107,
-    VOL_DOWN  : 109,
-    RED    : 112,
-    GREEN  : 113,
-    YELLOW : 114,
-    BLUE   : 115,
-    SERVICES  : 120,
-    TV    : 121,
-    PHONE : 119,
-    EPG   : 119,
-    WEB   : 123,
-    APP   : 123,
-    REW   : 2066,
-    FFWD  : 2070,
-    PLAY  : 2082,
-    PAUSE : 2082,
-    CONTINUE : 2082,
-    STOP  : 2083,
-    REC   : 2087,
-    INFO  : 2089,
-    FRAME : 117,
-    AUDIO : 2071,
-    NULL  : 2076,
-    KEYBOARD  : 2076,
-    USB_UNMOUNTED : 2081,
-    USB_MOUNTED   : 2080
+    BACK  : 8, // Backspace
+    CHANNEL_PREV  : 1009, // Shift+Tab
+    CHANNEL_NEXT  : 9, // Tab
+    EXIT  : 27, // Esc
+    REFRESH  : 116, // F5
+    UP    : 38, // UP ARROW
+    DOWN  : 40, // DOWN ARROW
+    LEFT  : 37, // LEFT ARROW
+    RIGHT : 39, // RIGHT ARROW
+    OK    : 13, // Enter
+    PAGE_NEXT : 34, // Page Down
+    PAGE_PREV : 33, // Page Up
+    NEXT  : 34, // Page Down
+    PREV  : 33, // Page Up
+    VOL_UP    : 107, // NUMPAD +
+    VOL_DOWN  : 109, // NUMPAD -
+    RED    : 112, // F1
+    GREEN  : 113, // F2
+    YELLOW : 114, // F3
+    BLUE   : 115, // F4
+    SERVICES  : 120, // F9
+    TV    : 121,  // F10
+    PHONE : 119, // F8
+    EPG   : 119, // F8
+    WEB   : 123, // F12
+    APP   : 123, // F12
+    REW   : 2066, // Alt+B
+    FFWD  : 2070, // Alt+F
+    PLAY  : 2082, // Alt+R
+    PAUSE : 2082, // Alt+R
+    CONTINUE : 2082, // Alt+R
+    STOP  : 2083, // Alt+S
+    REC   : 2087, // Alt+W
+    INFO  : 2089, // Alt+Y
+    FRAME : 117,  // F6
+    AUDIO : 2071, // Alt+G
+    NULL  : 2076, // Alt+L
+    KEYBOARD  : 2076, // Alt+L
+    USB_UNMOUNTED : 2081, // Alt+Q
+    USB_MOUNTED   : 2080  // Alt+P
 };
