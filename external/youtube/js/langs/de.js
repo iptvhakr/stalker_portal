f43ebffc0918b67e5679b536981a03aca35430c0
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2013-04-01 13:39:39 +0000

    Added de localization to youtube (issue #1704)

---
+++ external/youtube/js/langs/de.js
@@ -0,0 +1,107 @@
+var lang = {
+	"time": {
+		"ago"         : " vor",
+		"year"        : "Jahr",
+		"years2_4"    : " Jahr",
+		"years5_more" : " Jahr",
+		"month"       : " Monat",
+		"months2_4"   : " Monaten",
+		"months5_more": " Monaten",
+		"week"        : " Woche",
+		"weeks2_4"    : " Wochen",
+		"weeks5_more" : " Wochen",
+		"today"       : "heute",
+		"yesterday"   : "gestern",
+		"days2_4"     : " Tages",
+		"days5_more"  : " Tag"
+	},
+	"views": "Aufrufe: ",
+	"count": {
+		"thousands": " k.",
+		"millions" : " mln."
+	},
+	"loading": "Loading...",
+	"quality": {
+		"low"   : "Low",
+		"middle": "Middle",
+		"high"  : "High"
+	},
+	"f_category": "Kategorie",
+	"f_quality" : "Qualität",
+	"f_mode"    : "Play-Modus",
+	"f_search"  : "Suchen",
+	"cats"      : {
+		"all"           : "Alle Kategorien",
+		"most_popular"  : "Beliebteste",
+		"most_viewed"   : "Meistgesehene",
+		"top_favorites" : "TOP Favoriten",
+		"top_rated"     : "TOP Umfragen",
+		"most_recent"   : "Neu hochgeladenen",
+		"most_discussed": "Meist diskutiert",
+		"most_linked"   : "Meiste verschickt",
+		"most_responded": "Mit der größten Anzahl von Antworten",
+		"autos"         : "Autos und Fahrzeuge",
+		"animals"       : "Tiere",
+		"entertainment" : " Unterhaltung ",
+		"gaming"        : "Spiele",
+		"people"        : "Leute und Blogs",
+		"music"         : " Musik ",
+		"technology"    : "Wissenschaft und Technik ",
+		"news"          : "Nachrichten und Politik",
+		"education"     : "Bildung",
+		"howto"         : "Praktische Tipps und Styling",
+		"activism"      : "Soziales Engagement",
+		"travel"        : "Reisen und Events",
+		"sports"        : "Sport",
+		"film"          : "Film und Animation",
+		"comedy"        : "Komödie",
+		"nonprofit"     : "Soziales Engagement",
+		"search_result" : "Ergebnisse",
+		"playlist"      : "Playlist",
+		"playlists"     : "Playlists"
+	},
+	"intervals": {
+		"5"  : "5 sek",
+		"10" : "10 sek",
+		"20" : "20 sek",
+		"30" : "30 sek",
+		"60" : "1 min",
+		"300": "5 min",
+		"600": "10 min",
+		"900": "15 min"
+	},
+	"v_like": {
+		"suggestion": "Vorschläge",
+		"favorites" : "Favoriten",
+		"uploads"   : "Uploads",
+		"playlists" : "Playlists"
+	},
+	"user"      : "Anwender",
+	"videolike" : "Vorschläge",
+	"settings"  : "Einstellungen",
+	"as_orderby": "Sortierung:",
+	"as_time"   : "Zeit:",
+	"adv_search": {
+		"time": {
+			"today"    : " Heute",
+			"thisWeek" : " Diese Woche",
+			"thisMonth": " Diesen Monat",
+			"allTime"  : "Irgendwann"
+		},
+		"orderBy": {
+			"relevance": "Relevanz",
+			"published": "Hochgeladen am",
+			"rating"   : "Bewertung",
+			"viewCount": "Anzahl der Aufrufe "
+		},
+		"userType": {
+			"favorites": "Favoriten",
+			"uploads"  : "Uploads",
+			"playlists": "Playlists"
+		}
+	},
+	"main_field_search"          : "Suchen Videos",
+	"main_field_users_video"     : "Benutzers Video",
+	"as_data"                    : "Datentyp:",
+	"main_field_playlists_search": "Suche nach Playlist"
+};
