54c5a7ca58771cd22d4b52e2707252b6ee56056a
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2013-05-22 12:31:01 +0300

    Added SEND_EVENT API resource (issue #1869)

---
+++ server/lib/restcommandsendevent.class.php
@@ -0,0 +1,53 @@
+<?php
+
+class RESTCommandSendEvent extends RESTCommand
+{
+    public function create(RESTRequest $request){
+
+        $stb_list = $request->getConvertedIdentifiers();
+
+        $event = new SysEvent();
+
+        if (empty($stb_list)){
+            $event->setUserListByMac('all');
+        }else{
+            $event->setUserListById($stb_list);
+        }
+
+        if ($request->getData('ttl')){
+            $event->setTtl($request->getData('ttl'));
+        }
+
+        switch ($request->getData('event')) {
+            case 'send_msg':
+                if ($request->getData('need_reboot')){
+                    $event->sendMsgAndReboot($request->getData('msg'));
+                }else{
+                    $event->sendMsg($request->getData('msg'));
+                }
+                break;
+            case 'reboot':
+                $event->sendReboot();
+                break;
+            case 'reload_portal':
+                $event->sendReloadPortal();
+                break;
+            case 'update_channels':
+                $event->sendUpdateChannels();
+                break;
+            case 'play_channel':
+                $event->sendPlayChannel($request->getData('channel'));
+                break;
+            case 'update_image':
+                $event->sendUpdateImage();
+                break;
+            case 'cut_off':
+                $event->sendCutOff();
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+}
