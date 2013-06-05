17c9f4b310ae40a9c24a5e04f236b67a05928e77
Aleksey Zhurbitsky <zhurbitsky@gmail.com>
2013-06-05 12:15:29 +0300

    Fixed security issue (issue #1987)

--- server/administrator/get_img.php
+++ server/administrator/get_img.php
@@ -1,3 +1,9 @@
 <?php
 
-echo file_get_contents($_GET['url']);
+session_start();
+include "./common.php";
+moderator_access();
+
+if (strpos($_GET['url'], 'http://') === 0 && strpos($_GET['url'], 'kinopoisk.ru/')){
+    echo file_get_contents($_GET['url']);
+}
