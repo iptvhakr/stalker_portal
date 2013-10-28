f22e3dd0a1f7143d2a2d5112d9d662a77af96782
alex <zhurbitsky@gmail.com>
2013-10-28 15:04:36 +0200

    Added EPG period setting (issue #2951)

--- server/tasks/clear_epg.php
+++ server/tasks/clear_epg.php
@@ -4,7 +4,7 @@
 */
 include "./common.php";
 
-$from_date = date("Y-m-d H:i:s", time() - 7*24*60*60);
+$from_date = date("Y-m-d H:i:s", time() - Config::getSafe('epg_history_weeks', 1)*7*24*60*60);
 
 Mysql::getInstance()->delete('epg', array('time<' => $from_date));
 
