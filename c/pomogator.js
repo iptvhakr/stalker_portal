2a5942053e5328a07e1eec2ffc6c27caf31c1ff4
alex <zhurbitsky@gmail.com>
2014-08-22 10:53:35 +0300

    Add pomogator app (issue #4353)

---
+++ c/pomogator.js
@@ -0,0 +1,17 @@
+/**
+ * Redirection to pomogator module.
+ */
+(function(){
+
+    main_menu.add('POMOGATOR', [], 'mm_ico_pomogator.png', function(){
+
+        stb.setFrontPanel('.');
+
+        var url = 'http://pomogator.od.ua/_stb/?referrer='+encodeURIComponent(window.location);
+
+        _debug('url', url);
+        window.location = url;
+    }, {layer_name : "zoombytv"});
+
+    loader.next();
+})();
