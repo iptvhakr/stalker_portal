5b6145593c9119894d20f9e759b4addb9f1f4052
alex <zhurbitsky@gmail.com>
2014-10-02 17:02:28 +0300

    Fix clear epg script (issue #4418)

--- server/tasks/clear_epg.php
+++ server/tasks/clear_epg.php
@@ -6,25 +6,15 @@ include "./common.php";
 
 $from_date = date("Y-m-d H:i:s", time() - Config::getSafe('epg_history_weeks', 1)*7*24*60*60);
 
-$from_id = Mysql::getInstance()
-    ->select('max(id) as max_id')
-    ->from('epg')
-    ->where(array('time<' => $from_date))
-    ->get()
-    ->first('max_id');
+Mysql::getInstance()->delete('epg', array('time<' => $from_date));
 
-if ($from_id){
+if (Config::getSafe('use_optimize_table', true)){
 
-    Mysql::getInstance()->delete('epg', array('id<' => $from_id));
-
-    if (Config::getSafe('use_optimize_table', true)){
-
-        Mysql::getInstance()->query('ALTER TABLE `epg` DROP INDEX `ch_id_time`');
-        Mysql::getInstance()->query('ALTER TABLE `epg` DROP INDEX `real_id`');
-        Mysql::getInstance()->query('OPTIMIZE TABLE `epg`');
-        Mysql::getInstance()->query('ALTER TABLE `epg` ADD INDEX `ch_id_time` (`ch_id`,`time`)');
-        Mysql::getInstance()->query('ALTER TABLE `epg` ADD INDEX `real_id` (`real_id`)');
-    }
+    Mysql::getInstance()->query('ALTER TABLE `epg` DROP INDEX `ch_id_time`');
+    Mysql::getInstance()->query('ALTER TABLE `epg` DROP INDEX `real_id`');
+    Mysql::getInstance()->query('OPTIMIZE TABLE `epg`');
+    Mysql::getInstance()->query('ALTER TABLE `epg` ADD INDEX `ch_id_time` (`ch_id`,`time`)');
+    Mysql::getInstance()->query('ALTER TABLE `epg` ADD INDEX `real_id` (`real_id`)');
 }
 
 echo 1;
