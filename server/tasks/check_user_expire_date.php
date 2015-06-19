45245efe3d57528af2710bcd6836e1a24ceeae16
massiw <massiw@inbox.ru>
2015-06-19 12:53:51 +0300

    Does not work automatically turn off STB on cron (issue #5555)

--- server/tasks/check_user_expire_date.php
+++ server/tasks/check_user_expire_date.php
@@ -5,13 +5,13 @@ include "./common.php";
 if (Config::getSafe('enable_internal_billing', false)){
     $ids = Mysql::getInstance()->from("`users`")
         ->where(array(
-            "(TO_DAYS(`expire_billing_date`) - TO_DAYS(NOW())) < 0 AND CAST(`expire_billing_date` AS CHAR) <> '0000-00-00 00:00:00' AND 1=" => 1,
+            "(TO_DAYS(`expire_billing_date`) - TO_DAYS(NOW()) - 1) < 0 AND CAST(`expire_billing_date` AS CHAR) <> '0000-00-00 00:00:00' AND 1=" => 1,
             'status'=>0))
         ->get()->all('id');
     Mysql::getInstance()->update("`users`", array('status'=>1), array(" `id` IN ('" . implode("', '", $ids) . "') AND 1=" => 1, 'status'=>0));
     $online = Middleware::getOnlineUsersId();
     $event = new SysEvent();
     $event->setUserListById(array_intersect($ids, $online));
-    $event->sendMsgAndReboot(_('Time of your account is expired'));
+    $event->sendCutOff();
 }
 ?>
