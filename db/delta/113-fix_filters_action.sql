85835e35d58898123dab301e6d9e374783e0368f
massiw <massiw@inbox.ru>
2015-09-08 13:14:01 +0300

    fix for UsersList filters sql

---
+++ db/delta/113-fix_filters_action.sql
@@ -0,0 +1,8 @@
+--
+
+UPDATE `adm_grp_action_access` SET `action_name`= 'users-filter-list' WHERE `action_name`= 'users-filters-list';
+UPDATE `adm_grp_action_access` SET `action_name`= 'users-filter-list-json' WHERE `action_name`= 'users-filters-list-json';
+
+--//@UNDO
+
+--
