ab35d56853d9bf7d8a581baa25e408f3d596d3c9
alex <zhurbitsky@gmail.com>
2015-10-13 16:44:53 +0300

    Fix tv statistics (issue #6046)

--- c/global.js
+++ c/global.js
@@ -53,7 +53,7 @@ function _log(action, param, content_id){
         "type"   : "stb",
         "action" : "log",
         "real_action" : action,
-        "param"  : encodeURIComponent(param),
+        "param"  : (typeof param == 'string' || param instanceof String) ? encodeURIComponent(param) : param,
         "content_id" : content_id,
         "tmp_type" : stb.get_current_place()
     };
