8e25faa7b3e7aaf467a5d498d4c8394f1243006f
massiw <massiw@inbox.ru>
2015-10-16 12:07:50 +0300

    Admin-panel dashboard  for reseller (issue #6051)

---
+++ db/delta/121-update_for_users_activity.sql
@@ -0,0 +1,10 @@
+--
+
+ALTER TABLE `users_activity` CHANGE COLUMN `users_online` `users_online` VARCHAR(512) NOT NULL DEFAULT '{}' ;
+UPDATE `users_activity` SET `users_online` = CONCAT("{'total':", `users_online`, "}"), `time` = `time`;
+
+--//@UNDO
+
+ALTER TABLE `users_activity` CHANGE COLUMN `users_online` `users_online` INT(11) NOT NULL DEFAULT '0' ;
+
+--
