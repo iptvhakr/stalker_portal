7769e9db24d155849cd30d7f4d6ec243c821de51
alex <zhurbitsky@gmail.com>
2015-10-20 18:14:04 +0300

    Change app icon source (issue #6065)

---
+++ db/delta/124-app_icons.sql
@@ -0,0 +1,15 @@
+--
+
+ALTER TABLE `apps` ADD COLUMN `icons` TEXT;
+
+INSERT INTO `apps` (`url`, `added`, `alias`, `name`) VALUES
+  ('https://github.com/StalkerApps/vk.music', NOW(), 'vk.music', 'vk.music'),
+  ('https://github.com/StalkerApps/exua', NOW(), 'ex.ua', 'ex.ua');
+
+--//@UNDO
+
+ALTER TABLE `apps` DROP COLUMN `icons`;
+
+TRUNCATE `apps`;
+
+--
--- server/api/ext_module.php
+++ server/api/ext_module.php
@@ -2,7 +2,7 @@
 
 include "./common.php";
 
-if (empty($_GET['name'])){
+if (empty($_GET['name']) || empty($_GET['mac'])){
     exit;
 }
 
@@ -23,13 +23,19 @@ if (!$app['installed']){
 
 header('Content-Type: application/x-javascript');
 
+$user_theme = Mysql::getInstance()->from('users')->where(array('mac' => $_GET['mac']))->get()->first('theme');
+$user_theme = empty($user_theme) || !array_key_exists($user_theme, Middleware::getThemes())
+    ? Mysql::getInstance()->from('settings')->get()->first('default_template')
+    : $user_theme;
+
+$icon = $app['app_url'].'/img/{0}/'.$app['icons'].'/'.($user_theme == 'default' ? '2010' : '2014').'.png'
 ?>
 /**
 * Redirection to <?= $app['name'] ?> module.
 */
 (function(){
 
-main_menu.add('<?= $app['name'] ?>', [], 'mm_ico_<?= $app['alias'] ?>.png', function(){
+main_menu.add('<?= $app['name'] ?>', [], '<?= $icon ?>', function(){
 
 var params = '';
 
