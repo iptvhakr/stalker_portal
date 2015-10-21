e45ea1b168cf7b0c3d472cbcb7d50feffd26cdb1
alex <zhurbitsky@gmail.com>
2015-10-21 18:34:46 +0300

    Fix apps names conflict (issue #6089)

--- server/api/ext_module.php
+++ server/api/ext_module.php
@@ -60,7 +60,7 @@ params = stb.add_referrer(params, this.module.layer_name);
 _debug('url', '<?= $app['app_url'] ?>'+params);
 
 window.location = '<?= $app['app_url'] ?>'+params;
-}, {layer_name : "<?= $app['alias'] ?>"});
+}, {layer_name : "external_<?= $app['alias'] ?>"});
 
 loader.next();
 })();
