72423f10d619e20d9233626557d2366be8c357f0
massiw <massiw@inbox.ru>
2015-10-26 17:19:34 +0200

    Add a filter content type in the "users"  (issue #6096)

---
+++ db/delta/125-add_media_type_filter.sql
@@ -0,0 +1,12 @@
+--
+
+INSERT INTO `filters` (`title`,       `description`,                            `method`,                `type`,       `values_set`,             `default`)
+       VALUES         ('Media type', 'Users by type of mediacontent  watching', 'getUsersByPlayingType', 'VALUES_SET', 'getUsersPlayingTypeSet', '0');
+
+UPDATE `filters` SET `title` = 'Streaming server' WHERE `method` = 'getUsersByUsingStreamServer';
+
+--//@UNDO
+
+DELETE FROM `filters` WHERE `method` = 'getUsersByPlayingType';
+
+--
