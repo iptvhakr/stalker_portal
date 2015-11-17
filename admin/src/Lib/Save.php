6305b6145e35b929f6c9c398b9edeabcaf1cca04
massiw <massiw@inbox.ru>
2015-11-17 13:13:52 +0200

    Do not add the logo image to the channel (issue #6215)

--- admin/src/Lib/Save.php
+++ admin/src/Lib/Save.php
@@ -1,5 +1,6 @@
 <?php
 namespace Lib;
+use Imagick;
 
 class Save extends \Lib\Base {
     private $_filename = null;
@@ -17,48 +18,44 @@ class Save extends \Lib\Base {
     private function _img_resize($src, $dest, $width, $height) {
     if (!file_exists($src) || empty($width) || empty($height)) { return false; };
 
+        try{
+            $icon = new \Imagick($src);
+        }catch(ImagickException $e){
+            $this->_error[] = $e->getMessage();
+            return false;
+        }
+
         $size = getimagesize($src);
 
         if ($size === false) { return false; };
 
-        $format = strtolower(substr($size['mime'], strpos($size['mime'], '/')+1));
-
-        $icfunc = "imagecreatefrom" . $format;
-        if (!function_exists($icfunc)) { return false; };
-        $itofunc = "image" . $format;
-        if (!function_exists($itofunc)) { return false; };
-
         $x_ratio = $width / $size[0];
         $y_ratio = $height / $size[1];
-
 
         $ratio       = min($x_ratio, $y_ratio);
         $use_x_ratio = ($x_ratio == $ratio);
 
         $new_width   = $use_x_ratio  ? $width  : floor($size[0] * $ratio);
         $new_height  = !$use_x_ratio ? $height : floor($size[1] * $ratio);
-        $new_left    = $use_x_ratio  ? 0 : floor(($width - $new_width) / 2);
-        $new_top     = !$use_x_ratio ? 0 : floor(($height - $new_height) / 2);
 
-        $isrc = $icfunc($src);
-        $idest = imagecreatetruecolor($width, $height);
+        if (!$icon->resizeImage($new_width, $new_height, Imagick::FILTER_LANCZOS, 1)){
+            return false;
+        }
 
-        imagefill($idest, 0, 0, imagecolorallocate($isrc, 255, 255, 255));
-        imagecopyresampled($idest, $isrc, $new_left, $new_top, 0, 0,
-            $new_width, $new_height, $size[0], $size[1]);
+        if (!$icon->writeImage($dest)){
+            return false;
+        }
 
-        
-        $itofunc($idest, $dest);
+        $icon->destroy();
 
-        imagedestroy($isrc);
-        imagedestroy($idest);
+        chmod($dest, 0666);
 
         return true;
     }
 
     public function save() {
         move_uploaded_file($this->_source, $this->_target);
-
+        @unlink($this->_source);
         return true;
     }
 
