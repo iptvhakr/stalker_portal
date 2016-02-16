036c7f35452485f8a6588900179e4cfc036d4610
massiw <massiw@inbox.ru>
2016-02-16 12:56:24 +0200

    Wishes for the new admin-panel: IPTV - add sql (issue #6568)

---
+++ db/delta/138-reset_mediaclaims_action.sql
@@ -0,0 +1,9 @@
+--
+
+INSERT INTO `adm_grp_action_access`
+          (`controller_name`, `action_name`,      `is_ajax`, `description`)
+VALUES    ('tv-channels',     'itv-reset-claims',         1, 'Reset media-claims');
+
+--//@UNDO
+
+--
