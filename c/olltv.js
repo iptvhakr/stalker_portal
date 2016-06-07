17a22c0939c4eaffb949462eeaa5c253126cb08a
alex <zhurbitsky@gmail.com>
2016-06-07 18:43:28 +0300

    Remove oll.tv application (issue #7404)

--- c/olltv.js
+++ c/olltv.js
@@ -1,38 +1,3 @@
-/**
- * Redirection to oll.tv module.
- */
 (function(){
-
-    if (stb.type == 'MAG200'){
-        loader.next();
-        return;
-    }
-
-    main_menu.add('OLL.TV', [], 'mm_ico_oll.png', function(){
-
-        var params = '';
-
-        if (stb.user['web_proxy_host']){
-            params += '?proxy=http://';
-            if (stb.user['web_proxy_user']){
-                params += stb.user['web_proxy_user']+':'+stb.user['web_proxy_pass']+'@';
-            }
-            params += stb.user['web_proxy_host']+':' +stb.user['web_proxy_port'];
-        }
-
-        stb.setFrontPanel('.');
-
-        if (!params){
-            params += '?';
-        }else{
-            params += '&';
-        }
-
-        params = stb.add_referrer(params, this.module.layer_name);
-
-        _debug('url', '/' + stb.portal_path + '/external/olltv/index.html'+params);
-        window.location = '/' + stb.portal_path + '/external/olltv/index.html'+params;
-    }, {layer_name : "olltv"});
-
     loader.next();
 })();
