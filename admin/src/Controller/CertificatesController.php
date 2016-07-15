dc9f2aec0336645cb80fd18561727b2491b4a71a
massiw <massiw@inbox.ru>
2016-07-15 17:35:49 +0300

    License Management in the admin - pre-commit 2 (issue #7715)

--- admin/src/Controller/CertificatesController.php
+++ admin/src/Controller/CertificatesController.php
@@ -19,9 +19,9 @@ class CertificatesController extends \Controller\BaseStalkerController {
             array('id' => 2, 'title' => '100'),
             array('id' => 3, 'title' => '500'),
             array('id' => 4, 'title' => '1 000'),
-            array('id' => 4, 'title' => '2 000'),
-            array('id' => 4, 'title' => '5 000'),
-            array('id' => 4, 'title' => '10 000')
+            array('id' => 5, 'title' => '2 000'),
+            array('id' => 6, 'title' => '5 000'),
+            array('id' => 7, 'title' => '10 000')
         );
 
     }
@@ -62,6 +62,36 @@ class CertificatesController extends \Controller\BaseStalkerController {
         $attribute = $this->getDropdownAttribute();
         $this->checkDropdownAttribute($attribute);
         $this->app['dropdownAttribute'] = $attribute;
+
+        $data_set = array();
+        $data_set_row = array(
+            'id' => 0,
+            'lic_count' => 0,
+            'cert_begin' => 0,
+            'cert_end' => 0,
+            'status' => 0);
+        $status_ids = $this->getFieldFromArray($this->app['allStatus'], 'id');
+        $status_ids = array_combine(array_values($status_ids), array_values($status_ids));
+        $lic_count_ids = $this->getFieldFromArray($this->app['allLicCount'], 'id');
+        $lic_count_ids = array_combine(array_values($lic_count_ids), array_values($lic_count_ids));
+
+
+        $start_1 = mktime(0,0,0,1,1,2016);
+        $end_1 = $start_2 = mktime(0,0,0,3,1,2016);
+        $end_2  = time();
+
+        for($i=1; $i<=10; $i++){
+            $data_set_row['id'] = $i;
+            $data_set_row['lic_count'] = array_rand($lic_count_ids);
+            $data_set_row['cert_begin'] = rand($start_1,$end_1);
+            $data_set_row['cert_end'] = rand($start_2,$end_2);
+            $data_set_row['status'] = array_rand($status_ids);
+            $data_set[] = $data_set_row;
+        }
+
+        $this->app['data_set'] = $data_set;
+        $this->app['lic_count_set'] = array_combine($lic_count_ids, $this->getFieldFromArray($this->app['allLicCount'], 'title'));
+        $this->app['status_set'] = array_combine($status_ids, $this->getFieldFromArray($this->app['allStatus'], 'title'));
 
         return $this->app['twig']->render($this->getTemplateName(__METHOD__));
     }
