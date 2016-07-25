e94d604adc027f0d7d279911a04f8e4b6868f1e5
alex <zhurbitsky@gmail.com>
2016-07-25 18:15:29 +0300

    Optimize apps list loading time (issue #7735)

---
+++ db/delta/168_launcher_apps_available_version.sql
@@ -0,0 +1,9 @@
+--
+
+ALTER TABLE `launcher_apps` ADD COLUMN `available_version` varchar(16) NOT NULL DEFAULT '' AFTER `current_version`;
+
+--//@UNDO
+
+ALTER TABLE `launcher_apps` DROP COLUMN `available_version`;
+
+--
