c3599829fdbdf2ea92e0ef51a1a2d2205e82874e
alex <zhurbitsky@gmail.com>
2016-08-16 10:29:12 +0300

    Fix smartlauncher apps manager

--- server/lib/smartlauncherappsmanager.class.php
+++ server/lib/smartlauncherappsmanager.class.php
@@ -567,7 +567,7 @@ class SmartLauncherAppsManager
             .Config::getSafe('launcher_apps_path', 'stalker_launcher_apps/')
             .($app['type'] == 'plugin' ? 'plugins/' : '')
             .$app['alias']
-            .'/'.$app['current_version'].'/packag e.json'));
+            .'/'.$app['current_version'].'/package.json'));
 
         if (!$info){
             return false;
