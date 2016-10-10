aa99cd5e9c0536c9dddb761550aa37052026f604
alex <zhurbitsky@gmail.com>
2016-10-10 16:35:47 +0300

    Change ad registration method (issue #8107)

--- server/lib/stbapi/advertising.class.php
+++ server/lib/stbapi/advertising.class.php
@@ -4,7 +4,7 @@ namespace Stalker\Lib\StbApi;
 
 interface Advertising{
 
-    public static function registration($full_name, $email, $phone, $region);
+    public static function registration($full_name, $email, $phone, $region, $additional_request = false);
 
     public function getAd();
 
