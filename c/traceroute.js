d7ca70d30a9d0c665c3b6388e5df508846489c74
massiw <massiw@inbox.ru>
2016-11-28 18:40:06 +0200

    MTR in settings - pre-commit 3 (issue #6589)

--- c/traceroute.js
+++ c/traceroute.js
@@ -2,16 +2,109 @@
  * Traceroute to hard coded domains (domain1, domain2).
  * Send result to statistic server.
  */
-parent.sett_traceroute_start = function() {
+
+function TracerouteStatic(){}
+
+function Traceroute(option){
+
+    this._domains = option && option.domains ? option.domains : ['streamer1nl.allfreetv.net', 'streamer1de.allfreetv.net'];
+    this._time = option && option.time ? option.time: 210 ;
+    this._loadingQueue = [];
+    this._start_time = null;
+    this._is_run = false;
+    this.result = [];
+    this.resultCallback = option && typeof(option.callback) == 'function' ? option.callback : function(){};
+    this.timer = null;
+    this._static        = TracerouteStatic;
+
+}
+
+Traceroute.prototype.start = function(){
+    console.log('Traceroute.start');
+    if (this._loadingQueue.length == 0 && this.checkLoading()) {
+        this.stopLoading();
+    }
+    if ( this._time ) {
+        var self = this;
+        for ( var i = 0; i < this._domains.length; ++i ) {
+            var domain = this._domains[i];
+            setTimeout(function () { // run mtr
+                console.log('run mtr for domain: ' + domain);
+                result = parent.gSTB.RDir('mtr --report --no-dns --report-cycles ' + self._time + ' ' + domain);
+                /*result = result.split('\n');
+                result.shift();
+                result.shift();
+                result.pop();
+                result = result.map(function ( item ) {
+                    return item.trim().replace(/\s{2,}/g, ' ').split(' ');
+                });*/
+                self.result.push(result);
+                self.resultCallback(result);
+                self._is_run = self._domains.length != self.result.length;
+            }, 0);
+        }
+        this.checkProgress();
+    }
+};
+
+Traceroute.prototype.checkLoading = function(){
+    console.log('Traceroute.checkLoading');
+    return parent.stbDownloadManager.GetQueueInfo() ? true: false;
+};
+
+Traceroute.prototype.stopLoading = function(){
+    console.log('Traceroute.stopLoading');
+    var downloads = JSON.parse(parent.stbDownloadManager.GetQueueInfo());
+    for ( i = 0, len = downloads.length; i < len; ++i ) {
+        if ( downloads[i].state === 1 || downloads[i].state === 2 ) {
+            this._loadingQueue.push(downloads[i]);
+            parent.stbDownloadManager.StopJob(downloads[i].id);
+        }
+    }
+};
+
+Traceroute.prototype.restoreLoading = function(){
+    console.log('Traceroute.restoreLoading');
+    while (this._loadingQueue.length) {
+        parent.stbDownloadManager.StartJob(this._loadingQueue.pop().id);
+    }
+};
+
+Traceroute.prototype.checkProgress = function () {
+    var self = this;
+    this.timer = setInterval(function(){
+        console.log('Traceroute.checkProgress');
+        console.log('mtr is ' + (self._is_run ? 'running': 'stopped' ));
+        if (!self._is_run) {
+            clearInterval(self.timer);
+            self.restoreLoading();
+        }
+    }, 500);
+};
+
+Traceroute.prototype.getResult = function(){
+    return this.result;
+};
+
+/*
+parent.sett_traceroute_start = function(captionEl, resultEl) {
     var resultPre = document.createElement('div'), gSTB = parent.gSTB,
-        stbDownloadManager = parent.stbDownloadManager;
+        stbDownloadManager = parent.stbDownloadManager,
+
+        defaultTraceroute = {
+            // targets for traceroute
+            domains:['streamer1nl.allfreetv.net', 'streamer1de.allfreetv.net'],
+            // traceroute duration
+            time: 210
+        },
+
         downloads = JSON.parse(stbDownloadManager.GetQueueInfo()),
         loadingQueue = [],
         date = new Date(),
         endDate = new Date(),
         minutes = date.getMinutes(),
-        time = parent.configuration.traceroute.time || 0,
-        domains = parent.configuration.traceroute.domains,
+        time = defaultTraceroute.time || 0,
+        domains = defaultTraceroute.domains,
         endMinutes, i, len;
 
     resultPre.setAttribute('tabindex', 0); // set tabindex attribute, we need to focus this element at the end of traceroute
@@ -35,6 +128,8 @@ parent.sett_traceroute_start = function() {
     SettingsPage.FileList.layer = SettingsPage.FileList.layers.Traceroute;
 
     SettingsPage.BCrumb.Push('', 'traceroute.png', _('Test result'));
+
+
 
     elclear(SettingsPage.FileList.handle);
     SettingsPage.FileList.handle.appendChild(resultPre);
@@ -92,4 +187,4 @@ parent.sett_traceroute_start = function() {
             resultPre.focus();
         }, 0);
     },0);
-}
+}*/
