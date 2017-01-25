3f6050d8564c94877ee15b0a3857e173785f3eed
massiw <massiw@inbox.ru>
2017-01-25 18:09:57 +0200

    Error when adding a TV channel in the base - pre-commit 1 (issue #10446)

--- db/delta/183-nimble_support.sql
+++ db/delta/183-nimble_support.sql
@@ -1,7 +1,7 @@
 --
 
 ALTER TABLE `ch_links` ADD COLUMN `nimble_auth_support` TINYINT DEFAULT 0;
-ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza', 'nimble') NULL DEFAULT NULL ;
+ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza', 'edgecast_auth', 'nimble') NULL DEFAULT NULL ;
 ALTER TABLE `storages` ADD COLUMN `nimble_dvr` TINYINT(4) NULL DEFAULT 0;
 ALTER TABLE `itv` ADD COLUMN `nimble_dvr` TINYINT(4) NULL DEFAULT 0;
 
@@ -9,7 +9,7 @@ ALTER TABLE `itv` ADD COLUMN `nimble_dvr` TINYINT(4) NULL DEFAULT 0;
 --//@UNDO
 
 ALTER TABLE `ch_links` DROP COLUMN `nimble_auth_support`;
-ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza') NULL DEFAULT NULL ;
+ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza', 'edgecast_auth') NULL DEFAULT NULL ;
 ALTER TABLE `storages` DROP COLUMN `nimble_dvr`;
 ALTER TABLE `itv` DROP COLUMN `nimble_dvr`;
 
--- db/delta/184-akamai_auth_support.sql
+++ db/delta/184-akamai_auth_support.sql
@@ -1,12 +1,12 @@
 --
 
 ALTER TABLE `ch_links` ADD COLUMN `akamai_auth_support` TINYINT DEFAULT 0;
-ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza', 'edgecast_auth', 'akamai_auth') NULL DEFAULT NULL ;
+ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza', 'edgecast_auth', 'nimble', 'akamai_auth') NULL DEFAULT NULL ;
 
 
 --//@UNDO
 
-ALTER TABLE `ch_links` DROP COLUMN `edgecast_auth_support`;
-ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza', 'edgecast_auth') NULL DEFAULT NULL ;
+ALTER TABLE `ch_links` DROP COLUMN `akamai_auth_support`;
+ALTER TABLE `video_series_files` CHANGE COLUMN `tmp_link_type` `tmp_link_type` ENUM('flussonic','nginx','wowza', 'edgecast_auth', 'nimble') NULL DEFAULT NULL ;
 
 --
